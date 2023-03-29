6ba93e733ad412bfb5feacad0f6e9e41cf35ab13
Roy Chanunkha <roychanunkha@gmail.com>
2023-03-29 09:47:51 +0200

    added voidable record

---
+++ app/models/voidable_record.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# This class is used to model a voidable record
+class VoidableRecord < ApplicationRecord
+  self.abstract_class = true
+
+  include Auditable
+  include Voidable
+
+  default_scope { where(voided: 0) }
+  scope :voided, -> { unscoped.where.not(voided: 0) }
+end
