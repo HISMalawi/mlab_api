a1cf2ed659430a2c70adde2c70fcf67b48d263d4
Hopson Gausi <hopgausi20@gmail.com>
2023-03-29 14:17:15 +0200

    prevent adding updated by column to default schema tables'

--- db/migrate/20230208125954_add_updated_by_column_to_every_table.rb
+++ db/migrate/20230208125954_add_updated_by_column_to_every_table.rb
@@ -1,7 +1,8 @@
 class AddUpdatedByColumnToEveryTable < ActiveRecord::Migration[7.0]
   def change
     ActiveRecord::Base.connection.tables.each do |table|
-      add_column_to_table(affected_table: table, col: 'updated_by')
+      column_names = ActiveRecord::Base.connection.columns(table).map(&:name)
+      add_column_to_table(affected_table: table, col: 'updated_by') if column_names.include?('updated_date')
     end
   end
   
