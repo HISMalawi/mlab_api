0de5df77c82d47fc9a147fc9fed3f9c294e053ed
Hopson Gausi <hopgausi20@gmail.com>
2023-03-30 10:28:49 +0200

    Fix Load users migration

--- bin/iblis/load_users_data.rb
+++ bin/iblis/load_users_data.rb
@@ -1,13 +1,5 @@
 Rails.logger = Logger.new(STDOUT)
 ActiveRecord::Base.transaction do
- # Load Roles
-  roles = Iblis.find_by_sql("SELECT * FROM  roles")
-  roles.each do |role|
-    if Role.find_by_name(role.name).nil?
-      Rails.logger.info("=========Loading Role: #{role.name}===========")
-      Role.create(name: role.name,  retired: 0, created_date: role.created_at, updated_date: role.updated_at)
-    end
-  end
   # Load Users
   users = Iblis.find_by_sql("SELECT * FROM users")
   users.each do |user|
@@ -25,19 +17,28 @@ ActiveRecord::Base.transaction do
     person = Person.where(first_name: first_name, last_name: last_name, sex: sex, created_date: user.created_at, updated_date: user.updated_at).first
     if person.nil?
       Rails.logger.info("=========Loading Person: #{user.name}===========")
-      person = Person.create(first_name: first_name, middle_name: middle_name, last_name: last_name, sex: sex, voided: 0, created_date: user.created_at, updated_date: user.updated_at)
+      person = Person.create!(first_name: first_name, middle_name: middle_name, last_name: last_name, sex: sex, created_date: user.created_at, updated_date: user.updated_at)
     end
     if UserService.username_exists? user.username
       mlab_user = User.find_by_username(user.username)
     else
       Rails.logger.info("=========Loading User: #{user.username}===========")
-      mlab_user = User.new(person_id: person.id, username: user.username, password: user.password, voided: 0, created_date: Time.now, updated_date: Time.now)
-      if mlab_user.save
+      mlab_user = User.new(person_id: person.id, username: user.username, password: user.password, is_active:0)
+      if mlab_user.save!
         if !user.deleted_at.nil?
           Rails.logger.info("=========Voiding  deleted User: #{user.username}===========")
-          mlab_user.update(voided: 1, voided_by: 1, voided_date: user.deleted_at, voided_reason: 'deleted', updated_date: user.updated_at)
+          mlab_user.update!(is_active: 1)
         end
       end 
+    end
+
+    # Load Roles
+    roles = Iblis.find_by_sql("SELECT * FROM  roles")
+    roles.each do |role|
+      if Role.find_by_name(role.name).nil?
+        Rails.logger.info("=========Loading Role: #{role.name}===========")
+        Role.create!(name: role.name, created_date: role.created_at, updated_date: role.updated_at, creator: mlab_user.id)
+      end
     end
     # Load User Role mapping
     user_roles_sql = "SELECT distinct(u.username), r.name AS role FROM assigned_roles ar INNER JOIN roles r on r.id = ar.role_id 
@@ -47,26 +48,30 @@ ActiveRecord::Base.transaction do
       role = Role.find_by_name(user_role.role)
       if UserRoleMapping.where(role_id: role.id, user_id: mlab_user.id).first.nil?
         Rails.logger.info("=========Loading User Role For: #{user_role.username}===========")
-        UserRoleMapping.create(role_id: role.id, user_id: mlab_user.id, retired: 0, created_date: Time.now, updated_date: Time.now)
+        UserRoleMapping.create!(role_id: role.id, user_id: mlab_user.id, creator: mlab_user.id)
       end
     end
   end
+
+  user_ = User.all.first
   # Load departments
   departments = Iblis.find_by_sql("SELECT * FROM test_categories")
   departments.each do |department|
     if Department.find_by_name(department.name).nil?
       Rails.logger.info("=========Loading department: #{department.name}===========")
-      Department.create(name: department.name, retired: 0, created_date: department.created_at, updated_date: department.updated_at)
+      Department.create!(name: department.name, created_date: department.created_at, updated_date: department.updated_at, creator: user_.id)
     end
   end
   # Map User to department
   users_departments = Iblis.find_by_sql("SELECT tc.name AS dept, u.username AS user FROM user_testcategory utc INNER JOIN test_categories tc ON tc.id = utc.test_category_id INNER JOIN users u ON u.id=utc.user_id")
   users_departments.each do |u_dpt|
-    user = User.find_by_username(u_dpt.user)
+    user = User.find_by_username(u_dpt.user)&.id
     department = Department.find_by_name(u_dpt.dept)
-    if UserDepartmentMapping.where(user_id: user.id, department_id: department.id).first.nil?
+    puts department
+    puts "================================================================"
+    if UserDepartmentMapping.where(user_id: user, department_id: department.id, creator: user_.id).first.nil?
       Rails.logger.info("=========Mapping user: #{u_dpt.user} to department: #{u_dpt.dept}===========")
-      UserDepartmentMapping.create(user_id: user.id, department_id: department.id, retired: 0, created_date: Time.now, updated_date: Time.now) unless user.nil?
+      UserDepartmentMapping.create!(user_id: user, department_id: department.id, creator: user_.id) unless user.nil?
     end
   end
   Role.update_all(creator: 1)
