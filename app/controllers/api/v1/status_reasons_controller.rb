379e91ed1664337dcdb4caa5316cb8afc4cb9efb
Hopson Gausi <hopgausi20@gmail.com>
2023-03-30 15:14:17 +0200

    Refactored status reasons

--- app/controllers/api/v1/status_reasons_controller.rb
+++ app/controllers/api/v1/status_reasons_controller.rb
@@ -1,63 +1,42 @@
-class Api::V1::StatusReasonsController < ApplicationController
-  before_action :set_status_reason, only: [:show, :update, :destroy]
-
-  def index
-    @status_reasons = StatusReason.where(retired: 0)
-    render json: @status_reasons
-  end
-  
-  def show
-    if @status_reason.nil?
-      render json: {error: true, message: MessageService::RECORD_NOT_FOUND}, status: :ok
-    else
-      render json: {error: false, message: MessageService::RECORD_RETRIEVED, status_reason: @status_reason}, status: :ok
-    end
-  end
-
-  def create
-    @status_reason = StatusReason.new(description: status_reason_params[:description], retired: 0, creator: User.current.id, created_date: Time.now, updated_date: Time.now)
-    if @status_reason.save
-      render json:  {error: false, message: MessageService::RECORD_CREATED, status_reason: @status_reason}, status: :created, location: [:api, :v1, @status_reason]
-    else
-      render json: {error: true, message: @status_reason.errors}, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    if @status_reason.nil?
-      render json: {error: true, message: MessageService::RECORD_NOT_FOUND}, status: :ok
-    else
-      if @status_reason.update(description: status_reason_params[:description], updated_date: Time.now)
-        render json: {error: false, message: MessageService::RECORD_UPDATED, status_reason: @status_reason}, status: :ok
-      else
-        render json: {error: true, message: @status_reason.errors}, status: :unprocessable_entity
+module Api
+  module V1
+    class StatusReasonsController < ApplicationController
+      before_action :set_status_reason, only: [:show, :update, :destroy]
+    
+      def index
+        @status_reasons = StatusReason.all
+        render json: @status_reasons
+      end
+      
+      def show
+        render json: @status_reason
+      end
+    
+      def create
+        @status_reason = StatusReason.create!(status_reason_params)
+        render json: @status_reason, status: :created
+      end
+    
+      def update
+        @status_reason.update!(status_reason_params)
+        render json: @status_reason
+      end
+    
+      def destroy
+        @status_reason.void(params[:retired_reason])
+        render json: @status_reason
+      end
+    
+      private
+    
+      def set_status_reason
+        @status_reason = StatusReason.find(params[:id])
+      end
+    
+      def status_reason_params
+        params.require(:status_reason).permit(:description)
       end
     end
-  end
-
-  def destroy
-    if @status_reason.nil?
-      render json: {error: true, message: MessageService::RECORD_NOT_FOUND}, status: :ok
-    else
-      if @status_reason.update(retired: 1, retired_by: User.current.id, retired_reason: status_reason_params[:retired_reason], retired_date: Time.now, updated_date: Time.now)
-        render json: {error: false, message: MessageService::RECORD_DELETED}, status: :ok
-      else
-        render json: {error: true, message: @status_reason.errors}, status: :unprocessable_entity
-      end
-    end
-  end
-
-  private
-
-  def set_status_reason
-    begin
-      @status_reason = StatusReason.find(params[:id])
-    rescue => e
-      @status_reason = nil
-    end
-  end
-
-  def status_reason_params
-    params.require(:status_reason).permit(:description, :retired_reason)
+    
   end
 end
