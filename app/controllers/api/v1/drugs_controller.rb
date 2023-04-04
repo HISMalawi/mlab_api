c63198e0e4e31a05d2ab3d641eb432b4749a488f
Hopson Gausi <hopgausi20@gmail.com>
2023-04-04 15:10:27 +0200

    Added pagination to drugs

--- app/controllers/api/v1/drugs_controller.rb
+++ app/controllers/api/v1/drugs_controller.rb
@@ -4,8 +4,8 @@ module Api
       before_action :set_drug, only: [:show, :update, :destroy]
     
       def index
-        @drugs = Drug.all
-        render json: @drugs
+        @drugs = Drug.all.page(params[:page]).per(params[:per_page])
+        render json: {drugs: @drugs, meta: PaginationService.pagination_metadata(@drugs)}
       end
       
       def show
