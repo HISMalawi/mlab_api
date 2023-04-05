9dad24f460d755b1d4631c114bbe16da579d5a88
Hopson Gausi <hopgausi20@gmail.com>
2023-04-05 11:53:54 +0200

    Fixed usermanagement

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,76 +1,83 @@
-class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-  skip_before_action :authorize_request, only: [:login, :application_login]
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show, :update, :destroy]
+      before_action :run_validations, only: [:create, :update]
+      before_action :check_username, only: [:create]
+      skip_before_action :authorize_request, only: [:login, :application_login]
+    
+      def index
+        @users = User.all.page(params[:page]).per(params[:per_page])
+        render json: UserManagement::UserService.serialize_users(@users)
+      end
+      
+      def show
+        render json: UserManagement::UserService.find_user(@user.id)
+      end
+    
+      def create
+        @user = UserManagement::UserService.create_user(user_params)
+        render json: UserManagement::UserService.find_user(@user.id), status: :created
+      end
+    
+      def update
+        UserManagement::UserService.update_user(@user, user_params)
+        unless @user.username == user_params[:user][:username]
+          UserManagement::UserService.change_username(@user, user_params[:user][:username])
+        end
+        unless user_params[:user][:password].blank?
+          UserManagement::UserService.update_password(@user, user_params[:user][:old_password], user_params[:user][:password])
+        end
+        render json: UserManagement::UserService.find_user(@user.id)
+      end
+    
+      def destroy
+        @user.deactivate
+        render json: {message: MessageService::RECORD_DELETED}
+      end
+    
+      def login
+        payload = UserManagement::AuthService.login(params[:username], params[:password], params[:department])
+        raise UnAuthorized, 'User is not authorized to access this department' if payload == false
+        raise UnAuthorized, 'Invalid username or password' if payload.nil?
+        render json: {authorization: payload}, status: :ok
+      end
+    
+      def application_login
+        payload = UserManagement::AuthService.application_login(params[:username], params[:password])
+        raise UnAuthorized, 'Invalid username or password' if payload.nil?
+        render json: {authorization: payload}, status: :ok
+      end
+    
+      def refresh_token
+        render json: {authorization: UserManagement::AuthService.refresh_token}, status: :ok
+      end
+    
+      private
+    
+      def set_user
+        @user = User.find(params[:id])
+      end
 
-  def index
-    @users = User.all
-    render json: @users
-  end
-  
-  def show
-    render json: @user
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: [:api, :v1, @user]
-    else
-      render json: @user.errors, status: :unprocessable_entity
+      def user_params
+        params.permit(user: %i[username password old_password], person: %i[first_name middle_name last_name sex date_of_birth], roles: [], departments: [])
+      end
+    
+      def run_validations
+        unless params.has_key?('departments') && params[:departments].is_a?(Array)
+          raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << " for departments"
+        end
+        unless params.has_key?('roles') && params[:roles].is_a?(Array)
+          raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << " for roles"
+        end
+      end
+      
+      def check_username
+        if UserManagement::UserService.username_exists?(params[:user][:username])
+          raise ActiveRecord::RecordNotUnique, "Username already exists"
+        end
+      end
     end
-  end
-
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user.destroy
-  end
-
-  def login
-    payload = UserService.login(params[:username], params[:password], params[:department])
-    if payload == false
-      error_message = 'User is not authorized to access this department'
-      render json: {errors: [error_message]}, status: :unauthorized
-    elsif payload.nil?
-      error_message = 'Invalid username or password'
-      render json: {errors: [error_message]}, status: :unauthorized
-    else
-      render json: {authorization: payload}, status: :ok
-    end
-  end
-
-  def application_login
-    payload = UserService.application_login(params[:username], params[:password])
-    if payload.nil?
-      error_message = 'Invalid username or password'
-      render json: {errors: [error_message]}, status: :unauthorized
-    else 
-      render json: {authorization: payload}, status: :ok
-    end
-  end
-
-  # TO DO TOKEN REFRESH _ 
-  def refresh_token
-    user = User.find()
-    payload = {token: UserService::jwt_token_encode(user_id: User.current.id), expiry_time: UserService::TOKEN_VALID_TIME,
-       user: UserService::find_user(User.current.id)}
-    render json: {authorization: payload}, status: :ok
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:role_id, :person_id, :username, :password, :last_password_changed, :voided, :voided_by, :voided_reason, :voided_date, :creator, :created_date, :updated_date)
+    
   end
 end
