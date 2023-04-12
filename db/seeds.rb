96c6b37dffda91f2efbf298d922ce0c28781ccb0
Hopson Gausi <hopgausi20@gmail.com>
2023-04-12 09:05:11 +0200

    Removed unnecessary identifiers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Rails.logger = Logger.new(STDOUT)
 # Client Identifier types
-client_identifier_types = ['phone', 'email', 'village', 'physical_address', 'npid', 'art_number']
+client_identifier_types = ['phone', 'physical_address', 'npid']
 client_identifier_types.each do |identifier_type|
   if ClientIdentifierType.find_by_name(identifier_type).nil?
     Rails.logger.info("Loading client identifier type #{identifier_type}")
