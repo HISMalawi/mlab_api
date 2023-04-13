94062eb181122022b0b51f93257753b9ae74239e
Hopson Gausi <hopgausi20@gmail.com>
2023-04-13 15:02:22 +0200

    search by short name

--- app/models/test_type.rb
+++ app/models/test_type.rb
@@ -3,6 +3,6 @@ class TestType < RetirableRecord
   validates :name, uniqueness: true, presence: true
 
   def self.search(search_term)
-    where("name LIKE '%#{search_term}%'")
+    where("name LIKE '%#{search_term}%' OR short_name LIKE '%#{search_term}%'")
   end
 end
