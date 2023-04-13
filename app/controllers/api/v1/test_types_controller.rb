ea6ccb4de1d78a8cbf363d51c4432b914eebf542
Hopson Gausi <hopgausi20@gmail.com>
2023-04-13 14:51:19 +0200

    Implement search test type by name

--- app/controllers/api/v1/test_types_controller.rb
+++ app/controllers/api/v1/test_types_controller.rb
@@ -5,7 +5,11 @@ module Api
       before_action :check_specimen_indicator_params, only: [:create, :update]
     
       def index
-        @test_types = TestType.all.page(params[:page]).per(params[:per_page])
+        if params[:search].blank?
+          @test_types = TestType.all.page(params[:page]).per(params[:per_page])
+        else
+          @test_types = TestType.search(params[:search]).page().per(params[:per_page])
+        end
         render json: { test_types: @test_types, meta: PaginationService.pagination_metadata(@test_types)}
       end
       
--- app/models/test_type.rb
+++ app/models/test_type.rb
@@ -1,4 +1,8 @@
 class TestType < RetirableRecord
   belongs_to :department
   validates :name, uniqueness: true, presence: true
+
+  def self.search(search_term)
+    where("name LIKE '%#{search_term}%'")
+  end
 end
