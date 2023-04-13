a2a248c47f4ded22c13af5561805399ddcf7e700
Hopson Gausi <hopgausi20@gmail.com>
2023-04-13 12:41:32 +0200

    Load iblis clients

--- load_iblis_data.sh
+++ load_iblis_data.sh
@@ -1,3 +1,4 @@
 #!bin/bash
-rails r bin/iblis/load_users_data.rb
-rails r bin/iblis/meta_data.rb
+rails r bin/iblis/load_users_data.rb &&
+rails r bin/iblis/meta_data.rb &&
+rails r bin/iblis/load_clients.rb
