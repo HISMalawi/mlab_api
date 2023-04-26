e3eb0460fd494eb464db40a8a451c2ebc7caa828
Roy Chanunkha <roychanunkha@gmail.com>
2023-04-26 15:55:24 +0200

    interfacer added

---
+++ app/controllers/api/v1/interfacer_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Interfacer Controller
+    class InterfacerController < ApplicationController
+      skip_before_action :authorize_request, only: [:update]
+      before_action :authenticate_driver
+
+      def fetch_results
+        render json: read_service.new(accession_number: params[:accession_number]).read, status: :ok
+      end
+
+      def result_available
+        result = read_service.new(accession_number: params[:accession_number]).read
+        render json: { message: 'success', result_available: result.present? }, status: :ok
+      end
+
+      def update
+        write_service.new(allowed_params).write
+        render json: { message: 'success' }, status: :ok
+      end
+
+      private
+
+      def allowed_params
+        params.permit(:accession_number, :machine_name, :measure_id, :result)
+      end
+
+      def write_service
+        MachineService::WriteService
+      end
+
+      def read_service
+        MachineService::ReadingService
+      end
+
+      def authenticate_driver
+        username = params.require(:PHP_AUTH_USER)
+        password = params.require(:PHP_AUTH_PW)
+
+        user = User.find_by!(username:)
+
+        render json: { message: 'Invalid Credentials' }, status: :unauthorized unless user.password_hash == password
+      end
+    end
+  end
+end
