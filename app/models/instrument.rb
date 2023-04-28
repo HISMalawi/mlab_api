9895b9abea4a644611a72b9729569bca07e587e1
bryan <brianmsyamboza@gmail.com>
2023-04-28 16:10:48 +0200

    add supported tests for instrument

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,8 +1,15 @@
 class Instrument < RetirableRecord
   validates :name, presence: true, uniqueness: true
   has_many :instrument_test_type_mapping
+  has_many :test_types, through: :instrument_test_type_mapping
 
   def as_json(options = {})
-    super(options.merge({ only: %i[id name description ip_address hostname can_perform created_date] }))
+    super(options.merge({ only: %i[id name description ip_address hostname can_perform created_date], methods: %i[supported_tests] }))
+  end
+
+
+
+  def supported_tests
+    test_types.pluck(:name).join(', ')
   end
 end
