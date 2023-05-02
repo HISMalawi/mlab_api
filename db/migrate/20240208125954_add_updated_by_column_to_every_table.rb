c05a253c378fe52c415a2fb2b63f8e71d37fb9b1
Senzenjani <smanjawira@gmail.com>
2023-05-02 09:02:43 +0200

    Fixed duplicate updated_by

--- db/migrate/20240208125954_add_updated_by_column_to_every_table.rb
+++ db/migrate/20240208125954_add_updated_by_column_to_every_table.rb
@@ -2,7 +2,9 @@ class AddUpdatedByColumnToEveryTable < ActiveRecord::Migration[7.0]
   def change
     ActiveRecord::Base.connection.tables.each do |table|
       column_names = ActiveRecord::Base.connection.columns(table).map(&:name)
-      add_column_to_table(affected_table: table, col: 'updated_by') if column_names.include?('updated_date')
+      if column_names.include?('updated_at') && !column_names.include?('updated_by')
+        add_column_to_table(affected_table: table, col: 'updated_by')
+      end
     end
   end
   
