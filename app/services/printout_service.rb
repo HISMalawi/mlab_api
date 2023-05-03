fdba14ac8f77ca42e47ab2ffa1d992876ec7e170
Senzenjani <smanjawira@gmail.com>
2023-05-03 13:56:20 +0200

    Barcode printing tracking number

--- app/services/printout_service.rb
+++ app/services/printout_service.rb
@@ -29,6 +29,7 @@ module PrintoutService
       label.draw_multi_text("#{person.date_of_birth}  #{person.sex}")
       label.draw_multi_text("#{order.tracking_number}")
       label.draw_multi_text("#{order.tracking_number} * #{order.tracking_number.scan(/\d+/).first.to_i}")
+      label.draw_barcode(250, 180, 0, 1, 5, 15, 120, false, order.tracking_number)
       label.draw_multi_text("Col: #{order.created_date.strftime("%d/%b/%Y %H:%M")} #{User.find(order.creator).username}")
       label.draw_multi_text(tests.join(", "))
       label.print(1)
