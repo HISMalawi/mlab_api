708d3f50ee26e37b22ee7e1415396b057ddcbbdf
bryan <brianmsyamboza@gmail.com>
2023-05-05 13:09:21 +0200

    format obs from culture

--- app/models/drug_susceptibility.rb
+++ app/models/drug_susceptibility.rb
@@ -2,4 +2,9 @@ class DrugSusceptibility < VoidableRecord
   belongs_to :test
   belongs_to :organism
   belongs_to :drug
+
+
+  def as_json(options = {})
+    super(options.merge({only: %i[organism_id test_id drug_id zone interpretation]}))
+  end
 end
