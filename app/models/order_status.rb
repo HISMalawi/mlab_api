6b8c15561a23e89904fa1180b1add48275e94970
Emmanuel Z.K. Nyondo <emmanuelnyondo@gmail.com>
2023-05-05 11:20:45 +0200

    fix: order and test statuses models factor  out creator

--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -1,7 +1,6 @@
 class OrderStatus < VoidableRecord 
   validates :status_id, presence: true 
   validates :status_reason_id, presence: true
-  validates :creator, presence: true
 
   belongs_to :order
   belongs_to :status
