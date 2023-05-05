aba3c0be03ce11698cb44fb951034df08eee8609
Emmanuel Z.K. Nyondo <emmanuelnyondo@gmail.com>
2023-05-05 11:22:39 +0200

    fix: add order status controller and order status service

---
+++ app/controllers/api/v1/order_statuses_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class OrderStatusesController < ApplicationController
+
+      def index 
+        render json: OrderStatus.all
+      end
+
+      def specimen_rejected 
+        render json: OrderStatusesService.update_order_status(order_params, "specimen-rejected")
+      end
+
+      def specimen_accepted 
+        render json: OrderStatusesService.update_order_status(order_params, "specimen-accepted")
+      end
+
+      def specimen_not_collected 
+        render json: OrderStatusesService.update_order_status(order_params, "specimen-not-collected")
+      end
+
+      private
+      def order_params 
+        params.require(:order_status).permit(:order_id, :status_id, :status_reason_id)
+      end
+
+    end
+  end
+end
