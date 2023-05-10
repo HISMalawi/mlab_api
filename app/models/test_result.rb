81f0f2e6940483c1d6d6e91dc2219414cb939440
kellworks <kchidothi@outlook.com>
2023-05-10 09:26:19 +0200

    Update test status on entering result to completed

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,4 +1,10 @@
 class TestResult < VoidableRecord
   belongs_to :test
   belongs_to :test_indicator
+
+  after_create :set_test_status_to_completed
+
+  def set_test_status_to_completed
+    TestStatus.create!(test_id: test.id, status_id: Status.find_by_name('completed').id, creator: User.current.id)
+  end
 end
