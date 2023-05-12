335dd368181bb0e3499c696101ab876f9d4ef700
Hopson Gausi <hopgausi20@gmail.com>
2023-05-12 11:28:18 +0200

    filter tests by start date and end date

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,39 +1,55 @@
-class Api::V1::TestsController < ApplicationController
-  def index
-    render json: paginate(test_service.find_tests(params[:search], params[:department_id],  params[:status]))
-  end
-  
-  def show
-    render json: Test.find(params[:id])
-  end
+# frozen_string_literal: true
 
-  def create
-    test = Test.create!(test_params)
-    render json: test, status: :created
-  end
+module Api
+  module V1
+    # Test controller
+    class TestsController < ApplicationController
+      def index
+        tests = paginate(
+          test_service.find_tests(
+            params[:search],
+            params[:department_id],
+            params[:status],
+            params[:start_date],
+            params[:end_date]
+          )
+        )
+        render json: tests
+      end
 
-  def report
-    from, to, order_id = params.values_at(:from, :to, :order_id)
-    render json: test_service.client_report(Client.find(params[:client_id]), from, to, order_id), status: :ok
-  end
+      def show
+        render json: Test.find(params[:id])
+      end
 
-  def update
-    Test.find(params[:id]).update!(test_params)
-    render json: Test.find(params[:id]), status: :ok
-  end
+      def create
+        test = Test.create!(test_params)
+        render json: test, status: :created
+      end
 
-  def destroy
-    @test.void(params[:retired_reason])
-    render json: {message: MessageService::RECORD_DELETED}
-  end
+      def report
+        from, to, order_id = params.values_at(:from, :to, :order_id)
+        render json: test_service.client_report(Client.find(params[:client_id]), from, to, order_id), status: :ok
+      end
 
-  private
+      def update
+        Test.find(params[:id]).update!(test_params)
+        render json: Test.find(params[:id]), status: :ok
+      end
 
-  def test_service
-    Tests::TestService.new
-  end
+      def destroy
+        @test.void(params[:retired_reason])
+        render json: { message: MessageService::RECORD_DELETED }
+      end
 
-  def test_params
-    params.permit(:specimen_id, :order_id, :test_type_id)
+      private
+
+      def test_service
+        Tests::TestService.new
+      end
+
+      def test_params
+        params.permit(:specimen_id, :order_id, :test_type_id)
+      end
+    end
   end
 end
--- app/services/tests/test_service.rb
+++ app/services/tests/test_service.rb
@@ -2,7 +2,7 @@
 
 module Tests
   class TestService
-    def find_tests(query, department_id = nil, test_status = nil)
+    def find_tests(query, department_id = nil, test_status = nil, start_date = nil, end_date = nil)
       tests = Test.joins(:test_type, :current_test_status, order: [encounter: [client: [:person]]])
       if query.present?
         tests = tests.where('test_types.name LIKE ? or test_types.short_name LIKE ?', "%#{query}%",
@@ -15,6 +15,7 @@ module Tests
       if department_id.present? && is_not_reception?(department_id)
         tests = tests.where(test_type_id: TestType.where(department_id:).pluck(:id))
       end
+      tests = filter_by_date(tests, start_date, end_date) if start_date.present?
       tests.order('orders.id DESC')
     end
 
@@ -56,6 +57,10 @@ module Tests
 
     def search_by_test_status(tests, query)
       tests.where('current_test_status.name = (?)', query.to_s)
+    end
+
+    def filter_by_date(tests, start_date, end_date)
+      tests.where(created_date: Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day)
     end
 
     def client_service
