677935b22788df4de0877b77094fcf596b612fc4
Hopson Gausi <hopgausi20@gmail.com>
2023-05-19 12:14:00 +0200

    Change test status to complete for specimen accepted action

--- app/services/order_statuses_service.rb
+++ app/services/order_statuses_service.rb
@@ -6,6 +6,7 @@ module OrderStatusesService
       person_talked_to = params[:person_talked_to] rescue nil
 
       status = Status.find_by(name: status_name)
+      raise ActiveRecord::RecordNotFound, "Couldn't find status #{status_name}" if status.nil?
       order_status = OrderStatus.find_or_create_by!({
         order_id: order_id,
         status_id: status[:id],
@@ -13,18 +14,17 @@ module OrderStatusesService
         person_talked_to: person_talked_to
       })
       
-      # if ["specimen-rejected", "specimen-accepted"].include?(status_name.downcase) 
-        # tests = Test.where(:order_id => order_id).pluck(:id)
-        # if tests.present?
-        #   tests.each do |tid| 
-        #     TestStatus.find_or_create_by!({
-        #       test_id: tid,
-        #       status_id: status[:id],
-        #       status_reason_id: status_reason_id
-        #     })
-        #   end
-        # end
-      # end
+      if ["specimen-accepted"].include?(status_name.downcase) 
+        tests = Test.where(:order_id => order_id).pluck(:id)
+        if tests.present?
+          tests.each do |tid| 
+            TestStatus.find_or_create_by!({
+              test_id: tid,
+              status_id: Status.find_by(name: 'pending')&.id
+            })
+          end
+        end
+      end
 
       order_status
     end
