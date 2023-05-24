b00f0c01f9faf8f5b39bddfbcf4ac1887b8b1b93
Hopson Gausi <hopgausi20@gmail.com>
2023-05-24 15:46:43 +0200

    CHange spelling of the test type indicator

--- app/models/test_indicator.rb
+++ app/models/test_indicator.rb
@@ -2,5 +2,5 @@
 
 class TestIndicator < RetirableRecord
   belongs_to :test_type
-  enum test_indicator_type: [:auto_complete, :free_text, :numeric, :alpa_numeric]
+  enum test_indicator_type: [:auto_complete, :free_text, :numeric, :alpha_numeric]
 end
