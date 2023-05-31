349ae5f6279f8f985ac0e703674e734159b1b4da
Hopson Gausi <hopgausi20@gmail.com>
2023-05-31 14:32:46 +0200

    Serialize hema report init

--- app/services/reports/moh/haematology.rb
+++ app/services/reports/moh/haematology.rb
@@ -5,12 +5,29 @@ module Reports
 
         def generate_report(year, department)
           counts = MohReport.select(
-            "COUNT(DISTINCT CASE WHEN test_type IN ('FBC', 'FBC(Paeds)') THEN test_id END) AS full_blood_count",
-            "COUNT(DISTINCT CASE WHEN test_type IN ('FBC', 'FBC(Paeds)') AND test_indicator_name = 'HGB' THEN test_id END) AS hgb_only_blood_donor_excluded"
+            "month",
+            "COUNT(DISTINCT CASE WHEN test_type IN ('FBC', 'FBC(Paeds)') THEN test_id END) AS fbc",
+            "COUNT(DISTINCT CASE WHEN test_type IN ('FBC', 'FBC(Paeds)') AND test_indicator_name = 'HGB' THEN test_id END) AS hgb_o_bd_excluded",
+            "COUNT(DISTINCT CASE WHEN test_type IN ('Hemoglobin', 'Heamoglobin','Haemoglobin') 
+              AND test_indicator_name IN ('Hemoglobin','Haemoglobin','HGB', 'Hb') THEN test_id END) AS hgb_only_Hemacue",
+            "COUNT(DISTINCT CASE WHEN test_type IN ('FBC', 'FBC(Paeds)', 'Hemoglobin', 'Heamoglobin','Haemoglobin') 
+              AND test_indicator_name IN ('Hemoglobin','Haemoglobin','HGB', 'Hb') AND result <= 6 THEN test_id END) AS patient_with_hb_less_or_equal_6",
           ).where(
             department: department, 
             year: year
-          ).first
+          ).group(:month)
+          serialize_report(counts)
+        end
+
+        def serialize_report(counts)
+          report = {}
+          counts.each do |count|
+          report[count.month] = {
+            full_blood_count: count.fbc,
+              hgb_only_blod_donor_excluded: count.hgb_o_bd_excluded
+            }
+          end
+          report 
         end
 
       end
