48eab15ab7896f0fe42bbcdc891e0de1d7e6213d
Hopson Gausi <hopgausi20@gmail.com>
2023-05-31 12:58:32 +0200

    Refactor moh report view

--- db/migrate/20240208125978_create_view_moh_report.rb
+++ db/migrate/20240208125978_create_view_moh_report.rb
@@ -16,18 +16,17 @@ class CreateViewMohReport < ActiveRecord::Migration[7.0]
         p.date_of_birth AS dob,
         fs.name AS ward,
         dp.name AS department
-      FROM
-        tests t
-        INNER JOIN test_types tt ON t.test_type_id = tt.id
-        INNER JOIN current_test_status cts ON cts.test_id = t.id
-        INNER JOIN test_indicators ti ON ti.test_type_id = tt.id
-        INNER JOIN test_results tr ON tr.test_id = t.id AND ti.id = tr.test_indicator_id
-        INNER JOIN orders o ON o.id = t.order_id
-        INNER JOIN encounters e ON e.id = o.encounter_id
-        INNER JOIN clients c ON c.id = e.client_id
-        INNER JOIN people p ON p.id = c.person_id
-        INNER JOIN facility_sections fs ON fs.id = e.facility_section_id
-        INNER JOIN departments dp ON dp.id = tt.department_id
+      FROM tests t
+      INNER JOIN test_types tt ON t.test_type_id = tt.id
+      INNER JOIN current_test_status cts ON cts.test_id = t.id
+      INNER JOIN test_indicators ti ON ti.test_type_id = tt.id
+      INNER JOIN test_results tr ON tr.test_id = t.id AND ti.id = tr.test_indicator_id
+      INNER JOIN orders o ON o.id = t.order_id
+      INNER JOIN encounters e ON e.id = o.encounter_id
+      INNER JOIN clients c ON c.id = e.client_id
+      INNER JOIN people p ON p.id = c.person_id
+      INNER JOIN facility_sections fs ON fs.id = e.facility_section_id
+      INNER JOIN departments dp ON dp.id = tt.department_id
       WHERE
         tr.value IS NOT NULL AND tr.value <> '' AND cts.status_id in (4, 5)
     SQL
