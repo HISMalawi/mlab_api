ce9a836ab1f57eb50a0728e5bbc9e4cc8e668a66
Hopson Gausi <hopgausi20@gmail.com>
2023-06-12 14:45:53 +0200

    Refactor controller

--- app/controllers/api/v1/test_results_controller.rb
+++ app/controllers/api/v1/test_results_controller.rb
@@ -1,52 +1,61 @@
-class Api::V1::TestResultsController < ApplicationController
-  def index
-    render json: TestResult.all
-  end
-  
-  def show
-    render json: TestResult.find(params[:id])
-  end
+# frozen_string_literal: true
 
-  def create
-    permitted = test_result_params
-    indicators = permitted[:test_indicators]
-    unless permitted.has_key?(:test_id) && permitted.has_key?(:test_indicators)
-      render json: {message: MessageService::MISSING_REQUIRED_PARAMETERS}, status: :bad_request and return
-    end
-    results = []
-    ActiveRecord::Base.transaction do
-      results = indicators.collect do |indicator_obj|
-        indicator = indicator_obj[:indicator]
-        value = indicator_obj[:value]
-        machine_name = indicator_obj[:machine_name]
-        unless TestIndicator.find_by_id(indicator).present?
-          render json: {message: "Indicator with id #{indicator} not does not exists"}, status: :bad_request and return
+# Api controller module
+module Api
+  # V1 API module
+  module V1
+    # Test Results controller module to handle saving of results
+    class TestResultsController < ApplicationController
+      def index
+        render json: TestResult.all
+      end
+
+      def show
+        render json: TestResult.find(params[:id])
+      end
+
+      def create
+        permitted = test_result_params
+        indicators = permitted[:test_indicators]
+        unless permitted.key?(:test_id) && permitted.key?(:test_indicators)
+          render json: { message: MessageService::MISSING_REQUIRED_PARAMETERS }, status: :bad_request and return
         end
-        test_result = TestResult.find_by(test_id: permitted[:test_id], test_indicator_id: indicator)
-        if test_result.nil?
-          TestResult.create!(test_id: permitted[:test_id], test_indicator_id: indicator, value: value, result_date: Time.now, machine_name: machine_name)
-        else
-          test_result.void('Edited')
-          TestResult.create!(test_id: permitted[:test_id], test_indicator_id: indicator, value: value, result_date: Time.now, machine_name: machine_name)
+
+        results = []
+        ActiveRecord::Base.transaction do
+          results = indicators.collect do |indicator_obj|
+            indicator = indicator_obj[:indicator]
+            value = indicator_obj[:value]
+            machine_name = indicator_obj[:machine_name]
+            unless TestIndicator.find_by_id(indicator).present?
+              render json: { message: "Indicator with id #{indicator} not does not exists" },
+                     status: :bad_request and return
+            end
+
+            test_result = TestResult.find_by(test_id: permitted[:test_id], test_indicator_id: indicator)
+            test_result&.void('Edited')
+            TestResult.create!(test_id: permitted[:test_id], test_indicator_id: indicator, value:,
+                               result_date: Time.now, machine_name:)
+          end
         end
+        render json: results
+      end
+
+      def update
+        @test_result.update!(test_result_params)
+        render json: @test_result
+      end
+
+      def destroy
+        @test_result.void(params[:retired_reason])
+        render json: { message: MessageService::RECORD_DELETED }
+      end
+
+      private
+
+      def test_result_params
+        params.permit(:test_id, test_indicators: %i[indicator value machine_name])
       end
     end
-    render json: results
-  end
-
-  def update
-    @test_result.update!(test_result_params)
-    render json: @test_result
-  end
-
-  def destroy
-    @test_result.void(params[:retired_reason])
-    render json: {message: MessageService::RECORD_DELETED}
-  end
-
-  private
-
-  def test_result_params
-    params.permit(:test_id, test_indicators: %i[indicator value machine_name])
   end
 end
