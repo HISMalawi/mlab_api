512598d71f2ee85a58c75beef8064c4a84f0939b
Hopson Gausi <hopgausi20@gmail.com>
2023-06-14 15:37:54 +0200

    Fix insert into report job

--- app/jobs/insert_into_report_raw_data_job.rb
+++ app/jobs/insert_into_report_raw_data_job.rb
@@ -2,6 +2,6 @@ class InsertIntoReportRawDataJob
   include Sidekiq::Job
 
   def perform(test_id)
-    ReportRawData.insert_data(test_id)
+    ReportRawData.insert_data(test_id: test_id)
   end
 end
