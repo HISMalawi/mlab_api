3b938320d4f4666cbed8b5c7c78dd1ade336f46f
Hopson Gausi <hopgausi20@gmail.com>
2023-06-14 11:18:28 +0200

    Added ability to view patient report based on client id only

--- app/services/tests/test_service.rb
+++ app/services/tests/test_service.rb
@@ -24,9 +24,12 @@ module Tests
       orders = Order.joins(encounter: [client: [:person]]).where(
         client: {id: client.id},
         encounter: { start_date: Date.parse(from).beginning_of_day..Date.parse(to).end_of_day }
-        ) if (order_id.nil? && !from.nil?)
+        ) if (order_id.blank? && !from.blank?)
+      orders = Order.joins(encounter: [client: [:person]]).where(
+          client: {id: client.id}) if (order_id.blank? && from.blank?)
       person = client.person.as_json(only: %i[id first_name middle_name last_name sex date_of_birth birth_date_estimated])
       client_identifiers = ClientIdentifier.where(client_id: client.id)
+      
       {
         client: {
           person: person,
