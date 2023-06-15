cc06866d013bce7a2e9961b6b7f9b27189a10f98
Hopson Gausi <hopgausi20@gmail.com>
2023-06-15 09:43:55 +0200

    Test records daily log report

--- app/controllers/api/v1/daily_reports_controller.rb
+++ app/controllers/api/v1/daily_reports_controller.rb
@@ -6,9 +6,15 @@ module Api
   module V1
     # Controller for handling Daily reports requests
     class DailyReportsController < ApplicationController
-
       def daily_log
-        render json: { message: 'daily'}
+        from = params[:from]
+        to = params[:to]
+        department = params[:department]
+        test_status = params[:test_status]
+        report_type = params[:report_type]
+        records = Reports::DailyReport::DailyLog
+            .generate_report(report_type,{from:, to:, test_status:, department:})
+        render json: records
       end
     end
   end
---
+++ app/services/reports/daily_report/daily_log.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# Module Reports
+module Reports
+  # Daily reports module
+  module DailyReport
+    # Module service for generating daily log reports
+    module DailyLog
+      class << self
+        def generate_report(report_type, options = {})
+          case report_type
+          when 'test_record'
+            test_record(options[:from], options[:to], options[:test_status], options[:department])
+          else
+            []
+          end
+          
+        end
+
+        def test_record(from, to, test_status, department)
+          from = from.present? ? from : Date.today
+          to = to.present? ? to : Date.today
+          test_status = test_status.present? ? "'#{test_status}'" : "'completed', 'verified'"
+          depart_condition = department.present? ? " AND rrd.department = '#{department}' " : ' '          
+          collection = ReportRawData.find_by_sql("
+            SELECT * FROM report_raw_data rrd INNER JOIN (
+              SELECT test_id, MAX(status_created_date) status_created_date FROM report_raw_data
+                GROUP BY test_id
+            ) mrrd ON mrrd.test_id = rrd.test_id AND rrd.status_created_date=mrrd.status_created_date
+            AND rrd.created_date BETWEEN '#{from}' AND '#{to}' #{depart_condition}
+            AND rrd.status_name IN (#{test_status})
+          ")
+          { 
+            from:,
+            to:,
+            test_status: test_status.gsub(/'/, "").split(", "),
+            department:,
+            data: serialize_test_record(collection)
+          }
+        end
+
+        def serialize_test_record(collection)
+          unique_hashes = {}
+          collection.each do |hash|
+            test_id = hash[:test_id]
+            result_date = hash[:result_date]
+            test_indicator_name = hash[:test_indicator_name]
+            performed_by = TestStatus.where(test_id:, status_id: 4).first
+            authorized_by = TestStatus.where(test_id:, status_id: 5).first
+            result = hash[:result]
+            if unique_hashes[test_id].nil?
+              unique_hashes[test_id] = {
+                test_id:,
+                patient_id: hash[:patient_no],
+                visit_no: hash[:encounter_id],
+                patient_name: hash[:patient_name],
+                accession_number: hash[:accession_number],
+                specimen: hash[:specimen],
+                receipt_date: hash[:order_status_created_date],
+                test: hash[:test_type],
+                test_status: hash[:status_name],
+                order_status: hash[:order_status_name],
+                performed_by: performed_by.nil? ? '' : User.find(performed_by.creator).person.fullname,
+                authorized_by: authorized_by.nil? ? '' : User.find(authorized_by.creator).person.fullname,
+                remarks: '',
+                result_date:,
+                results: { test_indicator_name => result }
+              }
+            else
+              unique_hashes[test_id][:results][test_indicator_name] = result
+            end
+          end
+          unique_hashes.values
+        end
+      end
+    end
+  end
+end
