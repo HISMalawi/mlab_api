07d3731bd336ff42f853e55652a809fecc729b36
Hopson Gausi <hopgausi20@gmail.com>
2023-06-16 10:12:20 +0200

    Innit client load

--- bin/iblis/load_clients.rb
+++ bin/iblis/load_clients.rb
@@ -1,12 +1,80 @@
-# last_patient_id = Iblis.find_by_sql("SELECT * FROM patients order by id DESC LIMIT 1")[0].id
-last_patient_id = 1000
-def run(start_from, step, last_patient_id)
-  clients = IblisService::LoadClientService.get_iblis_clients(start_from, step)
-  IblisService::LoadClientService.load_client(clients)
-  start_from = start_from + step + 1
-  if start_from < last_patient_id
-    run(start_from, step, last_patient_id)
-  end
+#  last_patient_id = Iblis.find_by_sql("SELECT * FROM patients order by id DESC LIMIT 1")[0].id
+# last_patient_id = 1000
+# def run(start_from, step, last_patient_id)
+#   clients = IblisService::LoadClientService.get_iblis_clients(start_from, step)
+#   IblisService::LoadClientService.load_client(clients)
+#   start_from = start_from + step + 1
+#   if start_from < last_patient_id
+#     run(start_from, step, last_patient_id)
+#   end
+# end
+
+# run(1, 10, last_patient_id)
+Rails.logger = Logger.new(STDOUT)
+def load_people(offset, limit)
+  Iblis.find_by_sql("
+    SELECT
+      p.id AS id,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(p.name, ' ', 1), ' ', -1) AS first_name,
+        null AS middle_name,
+        SUBSTRING_INDEX(p.name, ' ', -1) AS last_name,
+        CASE 
+            WHEN p.gender = 0 THEN 'M'
+            WHEN p.gender = 1 THEN 'F'
+        END AS sex,
+        COALESCE(p.dob, '0000-00-00') AS date_of_birth,
+        p.dob_estimated AS birth_date_estimated,
+      IF(p.deleted_at IS NOT NULL, 1, 0) AS voided,
+        NULL AS voided_by,
+        NULL AS voided_reason,
+        NULL AS voided_reason,
+        p.created_by AS creator,
+        p.created_at AS created_date,
+        p.updated_at AS updated_date,
+        p.created_by AS updated_by
+    FROM patients p
+  ")
+end
+def load_clients(offset, limit)
+  Iblis.find_by_sql("
+    SELECT
+      p.id AS id,
+      p.id AS person_id,
+      NULL AS uuid,
+      IF(p.deleted_at IS NOT NULL, 1, 0) AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_reason,
+      p.created_by AS creator,
+      p.created_at AS created_date,
+      p.updated_at AS updated_date,
+      p.created_by AS updated_by
+    FROM patients p
+  ")
 end
 
-run(1, 10, last_patient_id)
+def get_count
+  Iblis.find_by_sql("
+    SELECT
+     count(*) AS count
+    FROM patients p
+  ")[0]
+end
+
+Rails.logger.info("Starting to process people....")
+total_records = get_count.count
+batch_size = 1000
+offset = 0
+count = total_records
+loop do
+  records = load_people(offset, batch_size)
+  c_records = load_clients(offset, batch_size)
+  break if records.empty?
+  Rails.logger.info("Processing batch  #{offset} of #{total_records} people: Remaining - #{count}")
+  Person.upsert_all(records.map(&:attributes), returning: false) unless records.empty?
+  Client.upsert_all(c_records.map(&:attributes), returning: false) unless c_records.empty?
+  offset += batch_size
+  count -= 1000
+end
+
+
---
+++ bin/iblis/load_encounter.rb
@@ -0,0 +1,86 @@
+Rails.logger = Logger.new(STDOUT)
+
+def get_records(offset, limit)
+  Iblis.find_by_sql(
+    "SELECT
+      t.visit_id AS id,
+      v.patient_id AS client_id,
+      NULL AS facility_id,
+      NULL AS destination_id,
+      ovw.ward_id AS facility_section_id,
+      v.created_at AS start_date,
+      t.time_verified AS end_date,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      1 AS creator,
+      v.created_at AS created_date,
+      v.updated_at AS updated_date,
+      ovw.visit_type_id AS encounter_type_id,
+      t.created_by AS updated_by
+    FROM
+        (
+            SELECT DISTINCT visit_id, time_verified, created_by
+            FROM tests
+            ORDER BY visit_id
+            LIMIT #{limit} OFFSET #{offset}
+        ) AS t
+        INNER JOIN visits v ON v.id = t.visit_id
+        LEFT JOIN (
+            SELECT
+                w.name,
+                vw.visit_type_id,
+                vw.ward_id,
+                iv.name visit_type
+            FROM
+                wards w
+                INNER JOIN visittype_wards vw ON vw.ward_id = w.id
+                INNER JOIN visit_types iv ON iv.id = vw.visit_type_id
+        ) ovw ON ovw.name = v.ward_or_location
+            AND ovw.visit_type = v.visit_type
+    ORDER BY t.visit_id"
+  )
+end
+
+def get_count
+  Iblis.find_by_sql(
+    "SELECT
+      count(visit_id) as count
+    FROM
+        (
+            SELECT DISTINCT visit_id, time_verified, created_by
+            FROM tests
+            ORDER BY visit_id
+        ) AS t
+        INNER JOIN visits v ON v.id = t.visit_id
+        LEFT JOIN (
+            SELECT
+                w.name,
+                vw.visit_type_id,
+                vw.ward_id,
+                iv.name visit_type
+            FROM
+                wards w
+                INNER JOIN visittype_wards vw ON vw.ward_id = w.id
+                INNER JOIN visit_types iv ON iv.id = vw.visit_type_id
+        ) ovw ON ovw.name = v.ward_or_location
+            AND ovw.visit_type = v.visit_type"
+  )[0]
+end
+Rails.logger.info("Starting to process....")
+total_records = get_count.count
+batch_size = 1000
+offset = 0
+count = total_records
+loop do
+  records = get_records(offset, batch_size)
+  break if records.empty?
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count}")
+  Encounter.upsert_all(records.map(&:attributes), returning: false) unless records.empty?
+  offset += batch_size
+  count -= 1000
+end
+
+
+
