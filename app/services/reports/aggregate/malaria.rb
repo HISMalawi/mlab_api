a5586a8ea43f43683cb748840420188e07db6274
Hopson Gausi <hopgausi20@gmail.com>
2023-06-19 10:42:06 +0200

    Aggregate report init

---
+++ app/services/reports/aggregate/malaria.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Reports module
+module Reports
+  # Aggregate reports module
+  module Aggregate
+    # Malaria reports module
+    module Malaria
+      class << self
+        
+      end
+    end
+  end
+end
