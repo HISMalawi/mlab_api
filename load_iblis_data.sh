be906b64ee7ee63c8d7d8a300848b8de7ad763af
Hopson Gausi <hopgausi20@gmail.com>
2023-06-19 09:13:38 +0200

    Add script execution

--- load_iblis_data.sh
+++ load_iblis_data.sh
@@ -6,4 +6,5 @@ rails r bin/iblis/meta_data.rb &&
 rails r bin/iblis/migrate_intruments.rb
 rails r bin/iblis/load_encounter_types.rb &&
 rails r bin/iblis/load_facility_data.rb &&
+rails r bin/iblis/load_encounter.rb &&
 rails r bin/iblis/load_test.rb
