0947e8331069b077c6ec297ab41c33f935c6b358
Hopson Gausi <hopgausi20@gmail.com>
2023-06-20 14:23:12 +0200

    Query by male and female

--- app/services/reports/aggregate/malaria.rb
+++ app/services/reports/aggregate/malaria.rb
@@ -8,9 +8,6 @@ module Reports
     module Malaria
       class << self
         def query_data_by_ward(from: nil, to: nil)
-          today = Date.today.strftime('%Y-%m-%d')
-          from = from.present? ? from : today
-          to = to.present? ? to : today
           ReportRawData.find_by_sql(
             "SELECT
             ward,
@@ -39,7 +36,7 @@ module Reports
                     AND result IN ('positive' , 'postive')
                     AND test_indicator_name = 'MRDT',
                 test_id,
-                NULL)) AS mrdt_post_under5,
+                NULL)) AS mrdt_pos_under5,
             COUNT(DISTINCT IF(age > 5
                     AND result IN ('No parasite seen' , 'No parasite',
                     'no parasites seen',
@@ -106,6 +103,102 @@ module Reports
           )
         end
 
+        def query_data_by_gender(from: nil, to: nil)
+          ReportRawData.find_by_sql(
+            "SELECT
+            gender,
+            COUNT(DISTINCT IF(age > 5 AND result = 'negative'
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_neg_over5,
+            COUNT(DISTINCT IF(age <= 5 AND result = 'negative'
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_neg_under5,
+            COUNT(DISTINCT IF(age > 5 AND result = 'invalid'
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_inv_over5,
+            COUNT(DISTINCT IF(age <= 5 AND result = 'invalid'
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_inv_under5,
+            COUNT(DISTINCT IF(age > 5
+                    AND result IN ('positive' , 'postive')
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_pos_over5,
+            COUNT(DISTINCT IF(age <= 5
+                    AND result IN ('positive' , 'postive')
+                    AND test_indicator_name = 'MRDT',
+                test_id,
+                NULL)) AS mrdt_pos_under5,
+            COUNT(DISTINCT IF(age > 5
+                    AND result IN ('No parasite seen' , 'No parasite',
+                    'no parasites seen',
+                    'nps',
+                    'NMPS',
+                    'no malaria palasite seen',
+                    'no malaria parasite seen')
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_neg_over5,
+            COUNT(DISTINCT IF(age <= 5
+                    AND result IN ('No parasite seen' , 'No parasite',
+                    'no parasites seen',
+                    'nps',
+                    'NMPS',
+                    'no malaria palasite seen',
+                    'no malaria parasite seen')
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_neg_under5,
+            COUNT(DISTINCT IF(age > 5
+                    AND result NOT IN ('No parasite seen' , 'No parasite',
+                    'no parasites seen',
+                    'nps',
+                    'NMPS',
+                    'no malaria palasite seen',
+                    'no malaria parasite seen')
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_pos_over5,
+            COUNT(DISTINCT IF(age <= 5
+                    AND result NOT IN ('No parasite seen' , 'No parasite',
+                    'no parasites seen',
+                    'nps',
+                    'NMPS',
+                    'no malaria palasite seen',
+                    'no malaria parasite seen')
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_pos_under5,
+            COUNT(DISTINCT IF(age > 5 AND result = 'invalid'
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_inv_over5,
+            COUNT(DISTINCT IF(age <= 5 AND result = 'invalid'
+                    AND test_indicator_name IN ('BLOOD FILM' , 'MALARIA SPECIES', 'RESULTS'),
+                test_id,
+                NULL)) AS micro_inv_under5
+            FROM
+                (SELECT
+                    TIMESTAMPDIFF(YEAR, dob, created_date) AS age,
+                        result,
+                        gender,
+                        test_indicator_name,
+                        test_id
+                FROM
+                  report_raw_data
+                WHERE
+                    test_type IN ('Malaria Screening' , 'Malaria Screening (Paeds)', 'Malaria Blood Film', 'MRDT ..', 'MRDT')
+                        AND status_id IN (4 , 5) AND created_date BETWEEN '#{from}' AND '#{to}'
+                GROUP BY dob , created_date , result , gender , test_indicator_name, test_id) AS t
+            GROUP BY gender
+          "
+          )
+        end
+
         def process_data_by_ward(record_data)
           summary = {
             total_tested: {
@@ -122,7 +215,7 @@ module Reports
                                                       data[:micro_inv_under5]
             summary[:total_tested][:mrdt_over_5] += data[:mrdt_neg_over5] + data[:mrdt_pos_over5] +
                                                     data[:mrdt_inv_over5]
-            summary[:total_tested][:mrdt_under_5] += data[:mrdt_neg_under5] + data[:mrdt_post_under5] +
+            summary[:total_tested][:mrdt_under_5] += data[:mrdt_neg_under5] + data[:mrdt_pos_under5] +
                                                      data[:mrdt_inv_under5]
           end
           summary
@@ -131,3 +224,8 @@ module Reports
     end
   end
 end
+
+
+# today = Date.today.strftime('%Y-%m-%d')
+#           from = from.present? ? from : today
+#           to = to.present? ? to : today
