6345e88b7cafe6e7ad785756732426f27104f63c
Hopson Gausi <hopgausi20@gmail.com>
2023-06-23 03:44:46 +0200

    Fix order statuses migration

--- bin/iblis/load_orders.rb
+++ bin/iblis/load_orders.rb
@@ -78,24 +78,26 @@ def iblis_orders_statuses(offset, limit, specimen_not_collected, specimen_accept
         WHEN s.specimen_status_id = 1 THEN #{specimen_not_collected}
         ELSE #{specimen_accepted}
       END AS status_id,
+      NULL AS status_reason_id,
       s.accepted_by AS creator,
-      s.accepted_by AS updated_by,
       0 AS voided,
       NULL AS voided_by,
       NULL AS voided_reason,
       NULL AS voided_date,
-      NULL AS status_reason_id,
-      NULL AS person_talked_to,
       CASE
           WHEN s.time_accepted IS NULL THEN t.time_created
-          WHEN s.time_accepted IS NULL AND t.time_created IS NULL THEN  NOW()
+          WHEN s.time_accepted IS NULL AND t.time_created IS NULL THEN '2016-01-01 06:06:06'
+          WHEN s.time_accepted = '0000-00-00 00:00:00' OR t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
           ELSE s.time_accepted
       END AS created_date,
       CASE
           WHEN s.time_accepted IS NULL THEN t.time_created
-          WHEN s.time_accepted IS NULL AND t.time_created IS NULL THEN  NOW()
+          WHEN s.time_accepted IS NULL AND t.time_created IS NULL THEN '2016-01-01 06:06:06'
+          WHEN s.time_accepted = '0000-00-00 00:00:00' OR t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
           ELSE s.time_accepted
-      END AS updated_date
+      END AS updated_date,
+      NULL AS person_talked_to,
+      s.accepted_by AS updated_by
     FROM
       specimens s
     INNER JOIN tests t ON t.specimen_id = s.id
@@ -118,8 +120,14 @@ def iblis_orders_status_rejected(offset, limit, specimen_rejected)
       NULL AS voided_date,
       rr.reason AS reason,
       s.reject_explained_to AS person_talked_to,
-      s.time_rejected AS created_date,
-      s.time_rejected AS updated_date
+      CASE
+          WHEN s.time_rejected = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE s.time_rejected
+      END AS created_date,
+      CASE
+          WHEN s.time_rejected = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE s.time_rejected
+      END AS updated_date
     FROM
       specimens s
     INNER JOIN rejection_reasons rr ON rr.id = s.rejection_reason_id
@@ -155,39 +163,39 @@ def orders_count
 end
 
 Rails.logger.info('Starting to process....')
-# total_records = orders_count.count
-# batch_size = 10_000
-# offset = 0
-# count = total_records
-# priority_id = Priority.find_or_create_by(name: 'Routine').id
-# loop do
-#   records = iblis_orders(offset, batch_size, priority_id)
-#   break if records.empty?
+total_records = orders_count.count
+batch_size = 10_000
+offset = 0
+count = total_records
+priority_id = Priority.find_or_create_by(name: 'Routine').id
+loop do
+  records = iblis_orders(offset, batch_size, priority_id)
+  break if records.empty?
 
-#   Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --ORDERS--  => (step 2 of 9)")
-#   unless records.empty?
-#     Order.upsert_all(records.map(&:attributes), returning: false)
-#   end
-#   offset += batch_size
-#   count -= batch_size
-# end
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --ORDERS--  => (step 2 of 9)")
+  unless records.empty?
+    Order.upsert_all(records.map(&:attributes), returning: false)
+  end
+  offset += batch_size
+  count -= batch_size
+end
 
-# total_records = iblis_orders_with_stat_count.count
-# batch_size = 10_000
-# offset = 0
-# count = total_records
-# priority_id = Priority.find_or_create_by(name: 'Stat').id
-# loop do
-#   records = iblis_orders_with_stat(offset, batch_size, priority_id)
-#   break if records.empty?
+total_records = iblis_orders_with_stat_count.count
+batch_size = 10_000
+offset = 0
+count = total_records
+priority_id = Priority.find_or_create_by(name: 'Stat').id
+loop do
+  records = iblis_orders_with_stat(offset, batch_size, priority_id)
+  break if records.empty?
 
-#   Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --Update Orders--  => (step 3 of 9)")
-#   unless records.empty?
-#     Order.upsert_all(records.map(&:attributes), returning: false)
-#   end
-#   offset += batch_size
-#   count -= batch_size
-# end
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --Update Orders--  => (step 3 of 9)")
+  unless records.empty?
+    Order.upsert_all(records.map(&:attributes), returning: false)
+  end
+  offset += batch_size
+  count -= batch_size
+end
 
 total_records = iblis_orders_stat_count.count
 batch_size = 10_000
@@ -202,25 +210,25 @@ loop do
 
   Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --Orders Statuses--  => (step 4 of 9)")
   unless records.empty?
-    OrderStatus.upsert_all(records.map(&:attributes), returning: false)
+    OrderStatus.insert_all(records.map(&:attributes), returning: false)
   end
   offset += batch_size
   count -= batch_size
 end
 
-# total_records = iblis_orders_reje_stat_count.count
-# batch_size = 10_000
-# offset = 0
-# count = total_records
-# specimen_rejected = Status.find_or_create_by(name: 'specimen-rejected').id
-# loop do
-#   records = iblis_orders_status_rejected(offset, batch_size, specimen_rejected)
-#   break if records.empty?
+total_records = iblis_orders_reje_stat_count.count
+batch_size = 10_000
+offset = 0
+count = total_records
+specimen_rejected = Status.find_or_create_by(name: 'specimen-rejected').id
+loop do
+  records = iblis_orders_status_rejected(offset, batch_size, specimen_rejected)
+  break if records.empty?
 
-#   Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --Rejected Orders Statuses--  => (step 5 of 9)")
-#   unless records.empty?
-#     OrderStatus.upsert_all(records.map { |record| record.attributes.merge('status_reason_id' => StatusReason.find_or_create_by(name: record.reason).id).except('reason') }, returning: false)
-#   end
-#   offset += batch_size
-#   count -= batch_size
-# end
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --Rejected Orders Statuses--  => (step 5 of 9)")
+  unless records.empty?
+    OrderStatus.upsert_all(records.map { |record| record.attributes.merge('status_reason_id' => StatusReason.find_or_create_by(description: record.reason).id).except('reason') }, returning: false)
+  end
+  offset += batch_size
+  count -= batch_size
+end
