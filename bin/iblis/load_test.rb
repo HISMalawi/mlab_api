bab7e7943708196794e23ace7244206df4b4b285
Hopson Gausi <hopgausi20@gmail.com>
2023-06-23 04:51:59 +0200

    Added test status loading

--- bin/iblis/load_test.rb
+++ bin/iblis/load_test.rb
@@ -16,8 +16,14 @@ def iblis_test(offset, limit)
       NULL AS voided_date,
       t.created_by AS creator,
       t.created_by AS updated_by,
-      t.time_created AS created_date,
-    t.time_created AS updated_date
+      CASE
+          WHEN t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_created
+      END AS created_date,
+      CASE
+        WHEN t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+        ELSE t.time_created
+      END AS updated_date
     FROM
       specimens s
     INNER JOIN
@@ -28,6 +34,143 @@ def iblis_test(offset, limit)
   ")
 end
 
+def iblis_created_test_status(offset, limit, created_status, creator)
+  Iblis.find_by_sql("
+    SELECT
+      t.id AS test_id,
+      #{created_status} AS status_id,
+      CASE
+        WHEN t.created_by = 0 THEN #{creator}
+        ELSE t.created_by
+      END AS creator,
+      CASE
+        WHEN t.created_by = 0 THEN #{creator}
+        ELSE t.created_by
+      END AS  updated_by,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      NULL AS status_reason_id,
+      NULL AS person_talked_to,
+      CASE
+          WHEN t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_created
+      END AS created_date,
+      CASE
+          WHEN t.time_created = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_created
+      END AS updated_date
+    FROM
+      tests t
+    WHERE t.time_created IS NOT NULL
+    LIMIT #{limit} OFFSET #{offset}
+  ")
+end
+
+def iblis_started_test_status(offset, limit, started_status, creator)
+  Iblis.find_by_sql("
+    SELECT
+      t.id AS test_id,
+      #{started_status} AS status_id,
+      CASE
+        WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS creator,
+      CASE
+        WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS  updated_by,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      NULL AS status_reason_id,
+      NULL AS person_talked_to,
+      CASE
+          WHEN t.time_started = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_started
+      END AS created_date,
+      CASE
+          WHEN t.time_started = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_started
+      END AS updated_date
+    FROM
+      tests t
+    WHERE t.time_started IS NOT NULL
+    LIMIT #{limit} OFFSET #{offset}
+  ")
+end
+
+def iblis_completed_test_status(offset, limit, completed_status, creator)
+  Iblis.find_by_sql("
+    SELECT
+      t.id AS test_id,
+      #{completed_status} AS status_id,
+      CASE
+        WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS creator,
+      CASE
+        WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS  updated_by,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      NULL AS status_reason_id,
+      NULL AS person_talked_to,
+      CASE
+          WHEN t.time_completed = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_completed
+      END AS created_date,
+      CASE
+          WHEN t.time_completed = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_completed
+      END AS updated_date
+    FROM
+      tests t
+    WHERE t.time_completed IS NOT NULL
+    LIMIT #{limit} OFFSET #{offset}
+  ")
+end
+
+def iblis_verified_test_status(offset, limit, verified_status, creator)
+  Iblis.find_by_sql("
+    SELECT
+      t.id AS test_id,
+      #{verified_status} AS status_id,
+      CASE
+        WHEN t.verified_by = 0 THEN #{creator}
+        ELSE t.verified_by
+      END AS creator,
+      CASE
+        WHEN t.verified_by = 0 THEN #{creator}
+        ELSE t.verified_by
+      END AS  updated_by,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      NULL AS status_reason_id,
+      NULL AS person_talked_to,
+      CASE
+          WHEN t.time_verified = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_verified
+      END AS created_date,
+      CASE
+          WHEN t.time_verified = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+          ELSE t.time_verified
+      END AS updated_date
+    FROM
+      tests t
+    WHERE t.time_verified IS NOT NULL
+    LIMIT #{limit} OFFSET #{offset}
+  ")
+end
+
+
 def iblis_test_count
   Iblis.find_by_sql("
     SELECT
@@ -37,17 +180,46 @@ def iblis_test_count
   ")[0]
 end
 
+creator = User.first.id
 Rails.logger.info('Starting to process....')
 total_records = iblis_test_count.count
-batch_size = 1_000
+batch_size = 10_000
 offset = 0
 count = total_records
 loop do
   records = iblis_test(offset, batch_size)
   break if records.empty?
 
-  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --TESTS-- step(4 of 9)")
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --TESTS-- step(6 of 9)")
   Test.upsert_all(records.map(&:attributes), returning: false) unless records.empty?
+  offset += batch_size
+  count -= batch_size
+end
+
+# process all test status records
+total_records = iblis_test_count.count
+batch_size = 10_000
+offset = 0
+count = total_records
+created_status = Status.find_or_create_by(name: 'pending').id
+started_status = Status.find_or_create_by(name: 'started').id
+completed_status = Status.find_or_create_by(name: 'completed').id
+verified_status = Status.find_or_create_by(name: 'verified').id
+loop do
+  records = []
+  created_records = iblis_created_test_status(offset, batch_size, created_status, creator)
+  started_records = iblis_started_test_status(offset, batch_size, started_status, creator)
+  completed_records = iblis_completed_test_status(offset, batch_size, completed_status, creator)
+  verified_records = iblis_verified_test_status(offset, batch_size, verified_status, creator)
+  records << started_records unless started_records.empty?
+  records << created_records unless created_records.empty?
+  records << completed_records unless completed_records.empty?
+  records << verified_records unless verified_records.empty?
+  break if records.empty?
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --TESTS STATUSES-- step(7 of 9)")
+  Parallel.map(records, in_processes: 4) do |record|
+    TestStatus.upsert_all(record.map(&:attributes), returning: false) unless record.empty?
+  end
   offset += batch_size
   count -= batch_size
 end
