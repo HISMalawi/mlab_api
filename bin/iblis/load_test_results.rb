3bde8a1df51af83f28ee6f23188fadf3e7d6e83d
Hopson Gausi <hopgausi20@gmail.com>
2023-06-23 06:43:50 +0200

    test results migration

---
+++ bin/iblis/load_test_results.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+Rails.logger = Logger.new($stdout)
+
+def iblis_test_result(offset, limit, creator)
+  Iblis.find_by_sql("
+    SELECT
+      tr.id,
+      tr.test_id,
+      tr.measure_id AS test_indicator_id,
+      tr.result AS value,
+      tr.device_name AS machine_name,
+      tr.time_entered AS result_date,
+      0 AS voided,
+      NULL AS voided_by,
+      NULL AS voided_reason,
+      NULL AS voided_date,
+      CASE
+            WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS creator,
+      CASE
+        WHEN t.tested_by = 0 THEN #{creator}
+        ELSE t.tested_by
+      END AS  updated_by,
+      CASE
+        WHEN tr.time_entered = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+        ELSE tr.time_entered
+      END AS created_date,
+      CASE
+        WHEN tr.time_entered = '0000-00-00 00:00:00' THEN '2016-01-01 06:06:06'
+        ELSE tr.time_entered
+      END AS updated_date
+    FROM
+      test_results tr
+          INNER JOIN
+      tests t ON tr.test_id = t.id
+    LIMIT #{limit} OFFSET #{offset}
+  ")
+end
+
+def iblis_test_result_count
+  Iblis.find_by_sql("
+    SELECT
+      count(*) AS count
+    FROM
+      test_results
+  ")[0]
+end
+
+creator = User.first.id
+Rails.logger.info('Starting to process....')
+total_records = iblis_test_result_count.count
+batch_size = 10_000
+offset = 0
+count = total_records
+loop do
+  records = iblis_test_result(offset, batch_size, creator)
+  break if records.empty?
+
+  Rails.logger.info("Processing batch #{offset} of #{total_records}: Remaining - #{count} --TEST RESULTS-- step(8 of 9)")
+  TestResult.upsert_all(records.map(&:attributes), returning: false) unless records.empty?
+  offset += batch_size
+  count -= batch_size
+end
