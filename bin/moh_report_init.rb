f71ea6a36e206e1ec7cc6aaa1bac8c29deac9cb1
Hopson Gausi <hopgausi20@gmail.com>
2023-06-26 14:04:10 +0200

    Run  4 processes in parallel

--- bin/moh_report_init.rb
+++ bin/moh_report_init.rb
@@ -1,8 +1,8 @@
 include Reports::Moh::MigrationHelpers::ExecuteQueries
 
-test_ids = Test.all.pluck('id')
-test_ids.reverse.each do |test_id|
-  puts "Loading into report data for #{test_id}"
+test_ids = Test.all.pluck('id').reverse
+Parallel.each(test_ids, in_processes: 4) do |test_id|
+  puts "Loading into report data for #{test_id} \n"
   insert_into_report_raw_data_table(test_id)
 end
 insert_into_moh_data_report_table(department: 'Haematology')
