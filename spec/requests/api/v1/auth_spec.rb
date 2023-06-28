bb0ed60cc5ad308140d4686d3ed3e4c65025b2ae
Hopson Gausi <hopgausi20@gmail.com>
2023-06-28 14:13:26 +0200

    refactor auth swagger

--- spec/requests/api/v1/auth_spec.rb
+++ spec/requests/api/v1/auth_spec.rb
@@ -1,10 +1,69 @@
-require 'swagger_helper'
+#frozen_string_literal: true
 
+require 'swagger_helper'
 
 TAG_NAME = 'Authentication'
 TAG_DESCRIPTION = 'Authenticating a user'
 
 RSpec.describe 'api/v1/auth', type: :request do
+  shared_examples 'authentication response' do
+    response '200', 'Authentication successful' do
+      schema type: 'object',
+        properties: {
+          token: { type: 'string' },
+          expiry_time: { type: 'string' },
+          user: {
+            type: 'object',
+            properties: {
+              id: { type: 'integer' },
+              username: { type: 'string' },
+              first_name: { type: 'string' },
+              middle_name: { type: 'string' },
+              last_name: { type: 'string' },
+              sex: { type: 'string' },
+              is_active: { type: 'boolean' },
+              date_of_birth: { type: 'string' },
+              birth_date_estimated: { type: 'string' },
+              voided: { type: 'integer' },
+              voided_reason: { type: 'string' },
+              roles: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'integer' },
+                    role_id: { type: 'integer' },
+                    role_name: { type: 'string' }
+                  }
+                }
+              },
+              departments: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'integer' },
+                    name: { type: 'string' },
+                    retired: { type: 'integer' },
+                    retired_reason: { type: 'string' }
+                  }
+                }
+              }
+            }
+          }
+        }
+      run_test!
+    end
+
+    response '401', 'UnAuthorized' do
+      schema type: 'object',
+        properties: {
+          error: { type: 'string' }
+        }
+      run_test!
+    end
+  end
+
   path '/api/v1/auth/login' do
     post('Login to get jwt key') do
       tags TAG_NAME
@@ -20,60 +79,7 @@ RSpec.describe 'api/v1/auth', type: :request do
         },
         required: %w[username password department]
       }
-      response '200', 'Authentication successful' do
-        schema type: 'object',
-          properties: {
-            token: { type: 'string' },
-            expiry_time: { type: 'string' },
-            user: {
-              type: 'object',
-              properties: {
-                id: { type: 'integer' },
-                username: { type: 'string' },
-                first_name: { type: 'string' },
-                middle_name: { type: 'string' },
-                last_name: { type: 'string' },
-                sex: { type: 'string' },
-                is_active: { type: 'boolean' },
-                date_of_birth: { type: 'string' },
-                birth_date_estimated: { type: 'string' },
-                voided: { type: 'integer' },
-                voided_reason: { type: 'string' },
-                roles: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      role_id: { type: 'integer' },
-                      role_name: { type: 'string' }
-                    }
-                  }
-                },
-                departments: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      name: { type: 'string' },
-                      retired: { type: 'integer' },
-                      retired_reason: { type: 'string' }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        run_test!
-      end
-      response '401', 'UnAuthorized' do
-        schema type: 'object',
-          properties: {
-            error: { type: 'string' }
-          }
-        run_test!
-      end
+      include_examples 'authentication response'
     end
   end
 
@@ -91,123 +97,17 @@ RSpec.describe 'api/v1/auth', type: :request do
         },
         required: %w[username password]
       }
-      response '200', 'Authentication successful' do
-        schema type: 'object',
-          properties: {
-            token: { type: 'string' },
-            expiry_time: { type: 'string' },
-            user: {
-              type: 'object',
-              properties: {
-                id: { type: 'integer' },
-                username: { type: 'string' },
-                first_name: { type: 'string' },
-                middle_name: { type: 'string' },
-                last_name: { type: 'string' },
-                sex: { type: 'string' },
-                is_active: { type: 'boolean' },
-                date_of_birth: { type: 'string' },
-                birth_date_estimated: { type: 'string' },
-                voided: { type: 'integer' },
-                voided_reason: { type: 'string' },
-                roles: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      role_id: { type: 'integer' },
-                      role_name: { type: 'string' }
-                    }
-                  }
-                },
-                departments: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      name: { type: 'string' },
-                      retired: { type: 'integer' },
-                      retired_reason: { type: 'string' }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        run_test!
-      end
-      response '401', 'UnAuthorized' do
-        schema type: 'object',
-          properties: {
-            error: { type: 'string' }
-          }
-        run_test!
-      end
+      include_examples 'authentication response'
     end
   end
 
   path '/api/v1/auth/refresh_token' do
     get('refresh_token auth') do
       tags TAG_NAME
-      description "Refresh token"
+      description 'Refresh token'
       security [bearerAuth: []]
       produces 'application/json'
-      response '200', 'Authentication successful' do
-        schema type: 'object',
-          properties: {
-            token: { type: 'string' },
-            expiry_time: { type: 'string' },
-            user: {
-              type: 'object',
-              properties: {
-                id: { type: 'integer' },
-                username: { type: 'string' },
-                first_name: { type: 'string' },
-                middle_name: { type: 'string' },
-                last_name: { type: 'string' },
-                sex: { type: 'string' },
-                is_active: { type: 'boolean' },
-                date_of_birth: { type: 'string' },
-                birth_date_estimated: { type: 'string' },
-                voided: { type: 'integer' },
-                voided_reason: { type: 'string' },
-                roles: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      role_id: { type: 'integer' },
-                      role_name: { type: 'string' }
-                    }
-                  }
-                },
-                departments: {
-                  type: 'array',
-                  items: {
-                    type: 'object',
-                    properties: {
-                      id: { type: 'integer' },
-                      name: { type: 'string' },
-                      retired: { type: 'integer' },
-                      retired_reason: { type: 'string' }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        run_test!
-      end
-      response '401', 'UnAuthorized' do
-        schema type: 'object',
-          properties: {
-            error: { type: 'string' }
-          }
-        run_test!
-      end
+      include_examples 'authentication response'
     end
   end
 end
