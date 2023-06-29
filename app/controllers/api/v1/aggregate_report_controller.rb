b5924ff622c569cf3dee4dae48f41e224217b86f
kellworks <kchidothi@outlook.com>
2023-06-29 13:42:04 +0200

    infection report

--- app/controllers/api/v1/aggregate_report_controller.rb
+++ app/controllers/api/v1/aggregate_report_controller.rb
@@ -29,7 +29,7 @@ module Api
         to = params[:to]
         department = params[:department]
         service = Reports::Aggregate::Infection.new
-        render json: { data: service.generate_report(from:, to:, department:) }
+        render json: { data: service.generate_report(from:, to:, department:), summary: service.get_summary(department:) }
       end
     end
   end
