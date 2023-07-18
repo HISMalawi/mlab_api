48dba86c5753f0728db125bb635125ab6d1c61e4
Hopson Gausi <hopgausi20@gmail.com>
2023-07-18 14:33:37 +0200

    Fix the query builder for reports

--- app/services/reports/moh/migration_helpers/execute_queries.rb
+++ app/services/reports/moh/migration_helpers/execute_queries.rb
@@ -13,12 +13,15 @@ module Reports
         def insert_into_moh_data_report_table(department:, action: 'init', time_filter: Date.today.to_s)
           queries = get_queries(department:, action:, time_filter:)
           queries = queries.join(' UNION ALL ')
-          records = ReportRawData.find_by_sql(queries)
-          return if records.empty?
+          sql_query = "SELECT MONTHNAME(created_date) AS month, SUM(total) AS total, indicator FROM (#{queries}) AS moh
+            GROUP BY MONTHNAME(created_date), indicator"
+          ReportRawData.find_by_sql(sql_query)
+          # records = ReportRawData.find_by_sql(queries)
+          # return if records.empty?
 
-          MohReportDataMaterialized.upsert_all(records.map do |record|
-                                                 record.attributes.except('id')
-                                               end, returning: false)
+          # MohReportDataMaterialized.upsert_all(records.map do |record|
+          #                                        record.attributes.except('id')
+          #                                      end, returning: false)
         end
 
         def insert_into_report_raw_data_table(test_id)
