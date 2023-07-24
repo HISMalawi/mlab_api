7886939658a6cb2074b2bd0cae8121ddff181b75
kellworks <kchidothi@outlook.com>
2023-07-24 15:07:41 +0200

    specimen registry

--- app/services/reports/aggregate/user_statistic.rb
+++ app/services/reports/aggregate/user_statistic.rb
@@ -26,11 +26,10 @@ module Reports
         users_test_counts = users.map do |user|
           tests_completed = TestStatus.where('created_date >= ? AND created_date <= ?', from, to).where(creator: user.id, status_id: Status.find_by_name('completed').id).count
           tests_received = Test.joins(order: :order_statuses).where(order_statuses: { creator: user.id }).where('order_statuses.status_id = ?', Status.find_by_name('specimen-accepted').id).count
-          specimen_collected = OrderStatus.where(creator: user.id, status_id: Status.find_by_name('pending').id).count
-          specimen_rejected = OrderStatus.where(creator: user.id, status_id: Status.find_by_name('specimen-rejected').id).count
-          tests_performed = TestStatus.where(creator: user.id, status_id: Status.find_by_name('verified').id).count
-          tests_authorized = TestStatus.where(creator: user.id, status_id: Status.find_by_name('verified').id).count
-
+          specimen_collected = OrderStatus.where('created_date >= ? AND created_date <= ?', from, to).where(creator: user.id, status_id: Status.find_by_name('pending').id).count
+          specimen_rejected = OrderStatus.where('created_date >= ? AND created_date <= ?', from, to).where(creator: user.id, status_id: Status.find_by_name('specimen-rejected').id).count
+          tests_performed = TestStatus.where('created_date >= ? AND created_date <= ?', from, to).where(creator: user.id, status_id: Status.find_by_name('verified').id).count
+          tests_authorized = TestStatus.where('created_date >= ? AND created_date <= ?', from, to).where(creator: user.id, status_id: Status.find_by_name('verified').id).count
           {
             user: user.username,
             tests_completed: tests_completed,
@@ -54,17 +53,19 @@ module Reports
       end
 
 
-      def specimen_registry(from: nil, to: nil, user: nil, page: nil, limit: nil)
+      def specimen_registry(from: nil, to: nil, user: nil, page: 1, limit: 10)
         users = user.nil? ? User.all : [User.find(user)]
         data = []
+        tests = []
         users.each do |creator|
-          data << ReportRawData.where(order_status_creator: creator.full_name).select(
+          data = ReportRawData.where(order_status_creator: creator.full_name).select(
             'test_id, test_type, specimen,  patient_no, patient_name, accession_number, created_date', 'id'
           ).distinct('test_id')
         end
-        tests =  PaginationService.paginate(data, page, limit)
-        # { 'metadata' => PaginationService.pagination_metadata(tests) }
-        tests
+        unless data.empty?
+          tests = PaginationService.paginate(data, page: page, limit: limit)
+        end
+        { 'tests' => tests, 'metadata' => PaginationService.pagination_metadata(tests) }
       end
 
       def tests_registry(from: nil, to: nil, user: nil, page: nil, limit: nil)
