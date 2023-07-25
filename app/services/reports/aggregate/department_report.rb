440cfd394cdf6e133da81c50d62d8d757b5eb2b5
Hopson Gausi <hopgausi20@gmail.com>
2023-07-25 09:53:07 +0200

    Add critical values to department report

--- app/services/reports/aggregate/department_report.rb
+++ app/services/reports/aggregate/department_report.rb
@@ -27,13 +27,54 @@ module Reports
 
       def generalize_depart_report
         data = ReportRawData.find_by_sql(@sql)
+        blood_bank_products = []
+        critical_values = %w[Haematology Biochemistry Paediatric].include?(@department) ? department_critical_values : []
         {
           from:,
           to:,
           department:,
           wards: wards(data),
-          data: serialize_generalize_depart_report(data)
+          data: serialize_generalize_depart_report(data),
+          critical_values:,
+          blood_bank_products:
         }
+      end
+
+      def blood_bank_product_report
+
+      end
+
+      def department_critical_values
+        sql_query = <<-SQL
+          SELECT
+          rrd.test_indicator_name,
+          rrd.ward,
+          CASE
+            WHEN ExtractNumberFromString(result) < tir.lower_range THEN 'Low'
+            WHEN ExtractNumberFromString(result) > tir.upper_range THEN 'High'
+            ELSE 'Normal'
+          END AS critical_value_level,
+          COUNT(DISTINCT rrd.test_id) AS count
+        FROM
+          report_raw_data rrd
+        INNER JOIN test_indicator_ranges tir ON
+          tir.test_indicator_id = rrd.test_indicator_id
+        WHERE
+          rrd.department = '#{department}'
+          AND rrd.created_date BETWEEN '#{from}' AND '#{to}'
+          AND rrd.status_id IN (4, 5)
+          AND (rrd.result IS NOT NULL OR rrd.result <> 0)
+        GROUP BY
+          rrd.test_indicator_name,
+          rrd.ward,
+          CASE
+            WHEN ExtractNumberFromString(result) < tir.lower_range THEN 'Low'
+            WHEN ExtractNumberFromString(result) > tir.upper_range THEN 'High'
+            ELSE 'Normal'
+          END
+        SQL
+        data = ReportRawData.find_by_sql(sql_query)
+        serialize_critical_values(data)
       end
 
       def serialize_generalize_depart_report(data)
@@ -45,6 +86,24 @@ module Reports
                                          .map do |test_type, test_type_entries|
                               {
                                 "test_type": test_type,
+                                "ward": test_type_entries.map do |entry|
+                                          { entry['ward'] => entry['count'] }
+                                        end.reduce({}, :merge)
+                              }
+                            end
+          }
+        end
+      end
+
+      def serialize_critical_values(data)
+        data.group_by { |entry| entry['test_indicator_name'].downcase }.map do |test_indicator_name, test_indicator_name_entries|
+          {
+            test_indicator_name.to_sym => test_indicator_name_entries.group_by do |entry|
+                              entry['critical_value_level'].downcase
+                            end
+                                         .map do |test_type, test_type_entries|
+                              {
+                                "critical_value_level": test_type,
                                 "ward": test_type_entries.map do |entry|
                                           { entry['ward'] => entry['count'] }
                                         end.reduce({}, :merge)
