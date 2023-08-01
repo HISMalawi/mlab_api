c1d52349db43b85ed729f48d6f1e2a0f225405a5
kellworks <kchidothi@outlook.com>
2023-08-01 10:55:05 +0200

    ast report generation

--- app/services/reports/aggregate/culture/ast.rb
+++ app/services/reports/aggregate/culture/ast.rb
@@ -4,6 +4,21 @@ module Reports
       class Ast
         def generate_report(month: nil, year: nil)
           data = {}
+          organisms = Organism.includes(:drugs)
+          drug_susceptibility_test_results = DrugSusceptibility.where('zone > ? AND MONTH(created_date) = ? AND YEAR(created_date) = ?', 1, month, year)
+          drug_counts_by_organism = drug_susceptibility_test_results.group(:organism_id, :drug_id).pluck(:organism_id, :drug_id, Arel.sql('COUNT(*) AS drug_count')).to_h { |(org_id, drug_id, count)| [[org_id, drug_id], count] }
+          organisms.each do |organism|
+            organism_name = organism.name
+            data[organism_name] = {
+              drugs: organism.drugs.map do |drug|
+                {
+                  drug_name: drug.name,
+                  count: drug_counts_by_organism[[organism.id, drug.id]].to_i
+                }
+              end
+            }
+          end
+          drug_susceptibility_test_results
         end
       end
     end
