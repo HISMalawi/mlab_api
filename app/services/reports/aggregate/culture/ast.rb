757738352240e07dbfc0ea7efa7b647c8601c7e4
kellworks <kchidothi@outlook.com>
2023-08-01 11:22:33 +0200

    refactor: ast

--- app/services/reports/aggregate/culture/ast.rb
+++ app/services/reports/aggregate/culture/ast.rb
@@ -3,22 +3,32 @@ module Reports
     module Culture
       class Ast
         def generate_report(month: nil, year: nil)
-          data = {}
+          data = []
           organisms = Organism.includes(:drugs)
-          drug_susceptibility_test_results = DrugSusceptibility.where('zone > ? AND MONTH(created_date) = ? AND YEAR(created_date) = ?', 1, month, year)
-          drug_counts_by_organism = drug_susceptibility_test_results.group(:organism_id, :drug_id).pluck(:organism_id, :drug_id, Arel.sql('COUNT(*) AS drug_count')).to_h { |(org_id, drug_id, count)| [[org_id, drug_id], count] }
+          drug_susceptibility_test_results = DrugSusceptibility.where('zone > ? AND MONTH(created_date) = ? AND YEAR(created_date) = ? AND interpretation != ?', 1, month, year, '')
+          interpretations_by_organism = drug_susceptibility_test_results.group(:organism_id, :drug_id, :interpretation).pluck(:organism_id, :drug_id, :interpretation, Arel.sql('COUNT(*) AS interpretation_count')).to_h { |(org_id, drug_id, interpretation, count)| [[org_id, drug_id, interpretation], count] }
           organisms.each do |organism|
             organism_name = organism.name
-            data[organism_name] = {
+            data << {
+              name: organism_name,
               drugs: organism.drugs.map do |drug|
+                drug_id = drug.id
+                interpretation_s_count = interpretations_by_organism[[organism.id, drug_id, 'S']].to_i
+                interpretation_r_count = interpretations_by_organism[[organism.id, drug_id, 'R']].to_i
+                interpretation_i_count = interpretations_by_organism[[organism.id, drug_id, 'I']].to_i
+
                 {
                   drug_name: drug.name,
-                  count: drug_counts_by_organism[[organism.id, drug.id]].to_i
+                  interpretations: {
+                    'S' => interpretation_s_count,
+                    'R' => interpretation_r_count,
+                    'I' => interpretation_i_count
+                  }
                 }
               end
             }
           end
-          drug_susceptibility_test_results
+          data
         end
       end
     end
