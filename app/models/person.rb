895bd47fe60aade60313a329b408f092a5b81bc6
kellworks <kchidothi@outlook.com>
2023-08-16 14:06:43 +0200

    patient search

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+require 'bantu_soundex'
+
 class Person < VoidableRecord
 
   def self.search(search_term)
     search_term = search_term.gsub(/\s+/, '')
-    where("CONCAT(first_name, middle_name, last_name) LIKE '%#{search_term}%' 
+    where("CONCAT(first_name, middle_name, last_name) LIKE '%#{search_term}%'
       OR CONCAT(last_name, middle_name, first_name) LIKE '%#{search_term}%'"
     )
   end
 
   def self.search_by_name_and_gender(first_name, last_name, gender)
-    where(first_name: first_name, last_name: last_name, sex: gender)
+    where(first_name_soundex: first_name.soundex, last_name_soundex: last_name.soundex, sex: gender)
   end
 
   def fullname
