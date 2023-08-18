d5c947abbfe6546a7d9e3f59f9bb8877f7dcf966
Hopson Gausi <hopgausi20@gmail.com>
2023-08-18 10:55:33 +0200

    set measure unit

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -7,8 +7,13 @@ class StockItem < VoidableRecord
   validates :name, uniqueness: true, presence: true
 
   before_save :strip_name_whitespace
+  before_save :set_measurement_unit
 
   private
+
+  def set_measurement_unit
+    self.measurement_unit = StockUnit.find(measurement_unit).id
+  end
 
   def strip_name_whitespace
     self.name = name.strip if name.present?
