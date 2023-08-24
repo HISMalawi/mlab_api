8270d27f946ea0ca621645b6883070d1891c1733
Hopson Gausi <hopgausi20@gmail.com>
2023-08-24 08:30:37 +0200

    swagger stock unit

--- spec/requests/api/v1/stock_unit_spec.rb
+++ spec/requests/api/v1/stock_unit_spec.rb
@@ -1,7 +1,98 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "Api::V1::StockUnits", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+require 'swagger_helper'
+
+TAG_NAME = 'Stock Unit'
+TAG_DESCRIPTION = 'Manage stock units'
+
+RSpec.describe 'api/v1/stock_units', type: :request do
+  path '/api/v1/stock_units' do
+    get('Display stock units details') do
+      tags TAG_NAME
+      description TAG_DESCRIPTION
+      security [bearerAuth: []]
+      produces 'application/json'
+      response(200, 'successful') do
+        schema type: :object,
+               properties: [{
+                id: { type: :integer },
+                name: { type: :string },
+                created_date: { type: :string }
+               }]
+        run_test!
+      end
+    end
+
+    post('create stock unit') do
+      tags TAG_NAME
+      description TAG_DESCRIPTION
+      consumes 'application/json'
+      produces 'application/json'
+      security [bearerAuth: []]
+      parameter name: :stock_unit, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ['name']
+      }
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                id: { type: :integer },
+                name: { type: :string },
+                created_date: { type: :string }
+              }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/stock_unit/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+    put('update stock unit') do
+      tags TAG_NAME
+      description TAG_DESCRIPTION
+      consumes 'application/json'
+      produces 'application/json'
+      security [bearerAuth: []]
+      parameter name: :stock_unit, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ['name']
+      }
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                id: { type: :integer },
+                name: { type: :string },
+                created_date: { type: :string }
+              }
+        run_test!
+      end
+    end
+
+    delete('delete stock unit') do
+      tags TAG_NAME
+      description TAG_DESCRIPTION
+      security [bearerAuth: []]
+      produces 'application/json'
+      parameter name: :stock_unit, in: :body, schema: {
+        type: :object,
+        properties: {
+          reason: { type: :string }
+        },
+        required: ['reason']
+      }
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                message: { type: :string }
+              }
+        run_test!
+      end
+    end
   end
 end
