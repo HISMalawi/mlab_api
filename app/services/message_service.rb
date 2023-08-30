19acdfd9e41662f845d8c886acfe158fc2ec319e
Hopson Gausi <hopgausi20@gmail.com>
2023-08-30 15:49:42 +0200

    Remove redundant RECORD_CREATED

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -15,5 +15,4 @@ module MessageService
   STOCK_REQUISITION_APPROVED = 'Stock Requisition Approved'
   STOCK_REQUISITION_NOT_COLLECTED = 'Stock Requisition Not Collected'
   STOCK_ORDER_RECEIVED = 'Stock Order Received'
-  RECORD_CREATED = 'Record created successfully'
 end
