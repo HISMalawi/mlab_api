79b0c6a0f8d01641155d4d3f8eafb6e9564db8aa
Hopson Gausi <hopgausi20@gmail.com>
2023-09-11 12:06:25 +0200

    Array pagination

--- app/services/pagination_service.rb
+++ app/services/pagination_service.rb
@@ -1,17 +1,21 @@
 module PaginationService
   class << self
-    def pagination_metadata(collection)
+    def pagination_metadata(paginated_collection)
       {
-        current_page: collection.current_page,
-        next_page: collection.next_page,
-        prev_page: collection.prev_page,
-        total_pages: collection.total_pages,
-        total_count: collection.total_count
+        current_page: paginated_collection.current_page,
+        next_page: paginated_collection.next_page,
+        prev_page: paginated_collection.prev_page,
+        total_pages: paginated_collection.total_pages,
+        total_count: paginated_collection.total_count
       }
     end
 
     def paginate(collection, page: 1, limit: 10)
       collection.page(page).per(limit)
+    end
+
+    def paginate_array(collection, page: 1, limit: 10)
+      Kaminari.paginate_array(collection).page(page).per(limit)
     end
   end
 end
