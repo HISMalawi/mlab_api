416e5760350c71c2404e8a63cb9b6c0fdd51d478
Hopson Gausi <hopgausi20@gmail.com>
2024-01-09 09:41:35 +0200

    Search by client id

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,7 +7,7 @@ class Person < VoidableRecord
   def self.search(search_term)
     search_term = search_term.gsub(/\s+/, '')
     where("CONCAT(first_name, middle_name, last_name) LIKE '%#{search_term}%'
-      OR CONCAT(last_name, middle_name, first_name) LIKE '%#{search_term}%'"
+      OR CONCAT(last_name, middle_name, first_name) LIKE '%#{search_term}%' OR id = #{search_term}"
     )
   end
 
