1ed9ced62fcc220604d6700e24a772711b33d7a5
Hopson Gausi <hopgausi20@gmail.com>
2024-02-12 11:08:46 +0200

    initial setup of prod env

--- app/jobs/moh_report_job.rb
+++ app/jobs/moh_report_job.rb
@@ -5,8 +5,8 @@ class MohReportJob
   include Sidekiq::Job
 
   def perform
-    report_years = Reports::Moh::ReportUtils.report_years.reverse
-    report_years.each do |year|
+    report_years = Reports::Moh::ReportUtils.report_years.reverse.first(2)
+    report_years[].each do |year|
       Report.find_or_create_by(name: 'moh_haematology', year:).update(
         data: Reports::MohService.generate_haematology_report(year)
       )
