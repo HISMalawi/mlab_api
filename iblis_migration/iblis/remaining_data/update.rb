b9aa55232f4a9234db538f487ef8dd7d80be4c11
Hopson Gausi <hopgausi20@gmail.com>
2024-02-22 12:15:58 +0200

    drugs suscept

--- iblis_migration/iblis/remaining_data/update.rb
+++ iblis_migration/iblis/remaining_data/update.rb
@@ -6,6 +6,7 @@ require_relative 'orders'
 require_relative 'tests'
 require_relative 'test_results'
 require_relative 'culture_observations'
+require_relative 'drug_suscept'
 
 client_id = 0
 test_id = 0
@@ -16,3 +17,4 @@ Orders.process_encounters(order_id)
 Tests.process_encounters(test_id)
 TestResults.process_test_results(test_id)
 CultureObservations.process_cs_observations(test_id)
+DrugSuscept.process_drug_susceptibilities(test_id)
