a31b36dc650b38b18cdd11b3f69b9aa81ba69d82
Hopson Gausi <hopgausi20@gmail.com>
2024-02-22 14:32:39 +0200

    refactoer migration of extra data

--- iblis_migration/iblis/remaining_data/update.rb
+++ iblis_migration/iblis/remaining_data/update.rb
@@ -9,13 +9,31 @@ require_relative 'culture_observations'
 require_relative 'drug_suscept'
 require_relative 'print_trails'
 
-client_id = 0
-test_id = 0
-order_id = 0
+def last_record
+  puts 'Enter last acession number from mlab:'
+  accession_number = gets.chomp
+  record = Order.where(accession_number:).first
+  if record.nil?
+    puts "\nPlease enter the valid accession number!!!\n\n"
+    last_record
+  else
+    date = record.created_date.to_date
+    accession_number = Order.where("DATE(created_date) BETWEEN '#{date - 4}' AND '#{date}'").first&.accession_number
+    last_iblis_record = Order.joins(:tests, :encounter).where(accession_number:)
+                           .select('orders.id, tests.id as test_id, encounters.client_id').first
+    last_iblis_record
+  end
+end
+
+last_record = last_record()
+client_id = last_record.client_id
+test_id = last_record.test_id
+order_id = last_record.id
+
 Clients.process_clients(client_id)
 Encounters.process_encounters(test_id)
-Orders.process_encounters(order_id)
-Tests.process_encounters(test_id)
+Orders.process_orders(order_id)
+Tests.process_tests(test_id)
 TestResults.process_test_results(test_id)
 CultureObservations.process_cs_observations(test_id)
 DrugSuscept.process_drug_susceptibilities(test_id)
