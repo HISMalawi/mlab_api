cd70e09d739b2ad40cff733c09aab60ad7101b5c
Hopson Gausi <hopgausi20@gmail.com>
2024-02-28 11:33:36 +0200

    fixed test panels not showing on tests

--- app/controllers/api/v1/specimen_controller.rb
+++ app/controllers/api/v1/specimen_controller.rb
@@ -1,13 +1,13 @@
 module Api
   module V1
     class SpecimenController < ApplicationController
-      before_action :set_specimen, only: [:show, :update, :destroy]
-    
+      before_action :set_specimen, only: %i[show update destroy]
+
       def index
         @specimen = Specimen.all
         render json: @specimen
       end
-      
+
       def show
         render json: @specimen
       end
@@ -15,29 +15,34 @@ module Api
       def specimen_test_type
         specimen = Specimen.find(params[:specimen_id])
         test_types = SpecimenTestTypeMapping.joins(:test_type).where(specimen_id: specimen.id)
-        test_types = filter_test_types_by_department(params[:department_id], test_types) if !params[:department_id].blank?
-        test_panel = TestTypePanelMapping.joins(:test_panel).joins(:test_type).where(test_type_id: test_types).pluck('test_panels.name')
-        test_types = test_types.pluck('name') + (test_panel)
+        unless params[:department_id].blank?
+          test_types = filter_test_types_by_department(params[:department_id],
+                                                       test_types)
+        end
+        test_panel = TestTypePanelMapping.joins(:test_panel).joins(:test_type).where(
+          test_type_id: test_types.pluck('specimen_test_type_mappings.test_type_id')
+        ).pluck('test_panels.name')
+        test_types = test_types.pluck('name') + test_panel
         render json: test_types.uniq
       end
-    
+
       def create
         @specimen = Specimen.create!(specimen_params)
         render json: @specimen, status: :created
       end
-    
+
       def update
         @specimen.update!(specimen_params)
         render json: @specimen
       end
-    
+
       def destroy
         @specimen.void(params[:retired_reason])
-        render json: {message: MessageService::RECORD_DELETED}
+        render json: { message: MessageService::RECORD_DELETED }
       end
-    
+
       private
-    
+
       def set_specimen
         @specimen = Specimen.find(params[:id])
       end
@@ -49,7 +54,7 @@ module Api
         end
         test_types
       end
-    
+
       def specimen_params
         params.require(:speciman).permit(:name, :description)
       end
