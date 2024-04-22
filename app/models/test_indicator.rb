3159a95e6177835c572f34315205a3e8ec55985b
Hopson Gausi <hopgausi20@gmail.com>
2024-04-22 13:21:18 +0200

    temporary fix to allow null values for test_type

--- app/models/test_indicator.rb
+++ app/models/test_indicator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TestIndicator < RetirableRecord
-  belongs_to :test_type
+  belongs_to :test_type, optional: true
   enum test_indicator_type: [:auto_complete, :free_text, :numeric, :alpha_numeric, :rich_text]
   has_many :test_indicator_ranges
 end
