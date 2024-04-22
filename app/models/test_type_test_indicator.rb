a7bb30a79074eccb3a9acc59632b048955bfbff2
Hopson Gausi <hopgausi20@gmail.com>
2024-04-22 16:18:20 +0200

    Creating the test type with the new test type indicator mapping

--- app/models/test_type_test_indicator.rb
+++ app/models/test_type_test_indicator.rb
@@ -3,6 +3,6 @@
 # test type indicator table
 class TestTypeTestIndicator < VoidableRecord
   self.table_name = 'test_type_indicator_mappings'
-  belongs_to :test_type
-  belongs_to :test_indicator
+  belongs_to :test_type, optional: true
+  belongs_to :test_indicator, optional: true
 end
--- app/services/test_catalog/test_types/create_service.rb
+++ app/services/test_catalog/test_types/create_service.rb
@@ -2,11 +2,11 @@ module TestCatalog
   module TestTypes
     module CreateService
       class << self
-
         def create_test_type(test_type_params, params)
           ActiveRecord::Base.transaction do
             test_type = TestType.create!(test_type_params)
             expected_tat = params.require(:expected_turn_around_time)
+            test_type.reload
             ExpectedTat.create!(
               test_type_id: test_type.id,
               value: expected_tat[:value],
@@ -22,13 +22,18 @@ module TestCatalog
         end
 
         def create_test_indicator(test_type_id, indicator_param)
-          indicator_param[:test_type_id] = test_type_id
-          test_indicator = TestIndicator.create!(JSON.parse(indicator_param.to_json).slice('name','unit','description','test_indicator_type', 'test_type_id'))
-          create_test_indicator_range(indicator_param[:indicator_ranges], test_indicator.id, indicator_param[:test_indicator_type])
+          test_indicator = TestIndicator.create!(JSON.parse(indicator_param.to_json).slice(
+                                                   'name', 'unit', 'description', 'test_indicator_type'
+                                                 ))
+          test_indicator.reload
+          TestTypeTestIndicator.find_or_create_by!(test_types_id: test_type_id, test_indicators_id: test_indicator.id)
+          create_test_indicator_range(indicator_param[:indicator_ranges], test_indicator.id,
+                                      indicator_param[:test_indicator_type])
         end
 
         def create_test_indicator_range(test_indicator_ranges, test_indicator_id, test_indicator_type)
-          if [TestCatalog::TestTypes::TestIndicatorType::AUTO_COMPLETE, TestCatalog::TestTypes::TestIndicatorType::ALPANUMERIC].include? test_indicator_type
+          if [TestCatalog::TestTypes::TestIndicatorType::AUTO_COMPLETE,
+              TestCatalog::TestTypes::TestIndicatorType::ALPANUMERIC].include? test_indicator_type
             check_indicator_range_params(test_indicator_ranges)
             test_indicator_ranges.each do |test_indicator_range|
               create_test_indicator_range_for_autocomplete(test_indicator_range, test_indicator_id)
@@ -45,34 +50,33 @@ module TestCatalog
           test_indicator_range[:test_indicator_id] = test_indicator_id
           TestIndicatorRange.create!(JSON.parse(test_indicator_range.to_json))
         end
-  
+
         def create_test_indicator_range_for_numeric(test_indicator_range, test_indicator_id)
           test_indicator_range[:test_indicator_id] = test_indicator_id
           TestIndicatorRange.create!(JSON.parse(test_indicator_range.to_json))
         end
 
         def check_indicator_range_params(test_indicator_ranges)
-          unless test_indicator_ranges && test_indicator_ranges.is_a?(Array)
-            raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << " for indicator_ranges"
-          end
+          return if test_indicator_ranges && test_indicator_ranges.is_a?(Array)
+
+          raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << ' for indicator_ranges'
         end
 
         def create_specimen_test_type_mapping(test_type_id, specimen_ids)
           specimen_ids.each do |specimen_id|
-            SpecimenTestTypeMapping.create!(specimen_id: specimen_id, test_type_id: test_type_id)
-          end
-        end
-  
-        def create_test_type_organism_mapping(test_type_id, organism_ids)
-          if organism_ids && organism_ids.is_a?(Array)
-            organism_ids.each do | organism |
-              TestTypeOrganismMapping.create!(organism_id: organism, test_type_id: test_type_id)
-            end
-          else
-            raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << " for organisms" 
+            SpecimenTestTypeMapping.create!(specimen_id:, test_type_id:)
           end
         end
 
+        def create_test_type_organism_mapping(test_type_id, organism_ids)
+          unless organism_ids && organism_ids.is_a?(Array)
+            raise ActionController::ParameterMissing, MessageService::VALUE_NOT_ARRAY << ' for organisms'
+          end
+
+          organism_ids.each do |organism|
+            TestTypeOrganismMapping.create!(organism_id: organism, test_type_id:)
+          end
+        end
       end
     end
   end
