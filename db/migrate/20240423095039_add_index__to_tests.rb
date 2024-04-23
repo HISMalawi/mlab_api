3a1994e0e5cca58c556f3640977925baf693dde8
Hopson Gausi <hopgausi20@gmail.com>
2024-04-23 11:54:28 +0200

    added indexes to tests table

---
+++ db/migrate/20240423095039_add_index__to_tests.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# migration to add indexes to the tests table
+class AddIndexToTests < ActiveRecord::Migration[7.0]
+  def change
+    add_index :tests, %i[voided status_id]
+    add_index :tests, %i[voided created_date]
+  end
+end
