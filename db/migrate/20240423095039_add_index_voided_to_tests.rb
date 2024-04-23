73bdd9f554020f578173afe5bd68bab6ba2c77c5
Hopson Gausi <hopgausi20@gmail.com>
2024-04-23 11:57:13 +0200

    rename migrate script file

--- db/migrate/20240423095039_add_index_voided_to_tests.rb
+++ db/migrate/20240423095039_add_index_voided_to_tests.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # migration to add indexes to the tests table
-class AddIndexToTests < ActiveRecord::Migration[7.0]
+class AddIndexVoidedToTests < ActiveRecord::Migration[7.0]
   def change
     add_index :tests, %i[voided status_id]
     add_index :tests, %i[voided created_date]
