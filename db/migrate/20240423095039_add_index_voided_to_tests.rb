53c816eec9b4e869ae20c6a4d74ed301a855e613
Hopson Gausi <hopgausi20@gmail.com>
2024-04-23 12:06:44 +0200

    update indexes

--- db/migrate/20240423095039_add_index_voided_to_tests.rb
+++ db/migrate/20240423095039_add_index_voided_to_tests.rb
@@ -5,5 +5,8 @@ class AddIndexVoidedToTests < ActiveRecord::Migration[7.0]
   def change
     add_index :tests, %i[voided status_id]
     add_index :tests, %i[voided created_date]
+    add_index :tests, %i[test_type_id created_date]
+    add_index :tests, %i[voided created_date]
+    add_index :tests, %i[voided test_type_id created_date status_id]
   end
 end
