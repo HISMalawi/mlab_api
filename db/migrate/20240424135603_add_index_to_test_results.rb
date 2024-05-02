a1109ee1accbb8ec75f4dece293ac6650f57d694
Hopson Gausi <hopgausi20@gmail.com>
2024-05-02 15:47:23 +0200

    Fix index duplication and long names

--- db/migrate/20240424135603_add_index_to_test_results.rb
+++ db/migrate/20240424135603_add_index_to_test_results.rb
@@ -3,6 +3,6 @@
 # AddIndexToTestResults migration
 class AddIndexToTestResults < ActiveRecord::Migration[7.0]
   def change
-    add_index :test_results, %i[voided test_indicator_id test_id]
+    add_index :test_results, %i[voided test_indicator_id test_id], name: 'idx_tr_vti_test_id'
   end
 end
