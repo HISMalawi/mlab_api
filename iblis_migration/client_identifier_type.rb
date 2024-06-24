9f614d06d120cae3bf6d71ec7f803104f23fe470
Hopson Gausi <hopgausi20@gmail.com>
2024-06-24 11:25:52 +0200

    add client metadata type metadata

--- iblis_migration/client_identifier_type.rb
+++ iblis_migration/client_identifier_type.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-client_identifier_types = %w[current_village current_district current_traditional_authority physical_address]
+client_identifier_types = %w[current_village current_district current_traditional_authority physical_address phone npid]
 client_identifier_types.each do |client_identifier_type|
   puts "Creating #{client_identifier_type} client identifier type"
   ClientIdentifierType.find_or_create_by(name: client_identifier_type)
