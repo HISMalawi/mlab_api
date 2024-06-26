ec6030a83edcca1159dca6fcd5ce809a5b269004
Hopson Gausi <hopgausi20@gmail.com>
2024-06-26 09:48:44 +0200

    refactor code

--- app/services/user_management/user_service.rb
+++ app/services/user_management/user_service.rb
@@ -3,28 +3,43 @@
 require 'bantu_soundex'
 
 module UserManagement
+  # User service module
+  # rubocop:disable Metrics/ModuleLength
   module UserService
+    # Class
+    # rubocop:disable Metrics/ClassLength
     class << self
-      def create_user(user_params)
+      # rubocop:disable Metrics/MethodLength
+      def create_user(params)
         ActiveRecord::Base.transaction do
-          person = Person.create!(user_params[:person])
-          person.update!(first_name_soundex: person.first_name.soundex, last_name_soundex: person.last_name.soundex)
-          user = User.new(user_params[:user])
+          person = create_person(params)
+          user = User.new(params[:user])
           user.person = person
           user.is_active = 0
-          user.password_hash = user_params[:user][:password]
+          user.password_hash = params[:user][:password]
           if user.save!
-            user_params[:roles].each do |role|
-              create_role(user.id, role)
-            end
-            user_params[:departments].each do |department|
-              create_department(user.id, department)
-            end
-            user_params[:lab_locations].each do |location|
-              create_lab_location(user.id, location)
-            end
+            create_user_extras(user.id, params[:roles], params[:departments], params[:lab_locations])
             user
           end
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      def create_person(params)
+        person = Person.create!(params[:person])
+        person.update!(first_name_soundex: person.first_name.soundex, last_name_soundex: person.last_name.soundex)
+        person
+      end
+
+      def create_user_extras(user_id, roles, departments, lab_locations)
+        roles.each do |role|
+          create_role(user_id, role)
+        end
+        departments.each do |department|
+          create_department(user_id, department)
+        end
+        lab_locations.each do |location|
+          create_lab_location(user_id, location)
         end
       end
 
@@ -40,19 +55,21 @@ module UserManagement
         UserLabLocationMapping.create!(user_id:, lab_location_id:)
       end
 
-      def update_user(user, user_params)
+      # rubocop:disable Metrics/AbcSize
+      def update_user(user, params)
         ActiveRecord::Base.transaction do
           person = user.person
-          updated_person = person.update!(user_params[:person])
-          if updated_person
-            person.update!(first_name_soundex: person.first_name.soundex, last_name_soundex: person.last_name.soundex)
-            update_roles(user.id, user_params[:roles])
-            update_departments(user.id, user_params[:departments])
-            update_lab_locations(user.id, user_params[:lab_locations])
-          end
+          updated_person = person.update!(params[:person])
+          return user unless updated_person
+
+          person.update!(first_name_soundex: person.first_name.soundex, last_name_soundex: person.last_name.soundex)
+          update_roles(user.id, params[:roles])
+          update_departments(user.id, params[:departments])
+          update_lab_locations(user.id, params[:lab_locations])
           user
         end
       end
+      # rubocop:enable Metrics/AbcSize
 
       def update_roles(user_id, role_ids)
         UserRoleMapping.where(user_id:).where.not(role_id: role_ids).each do |user_role_mapping|
@@ -106,15 +123,13 @@ module UserManagement
         user.save!
       end
 
-      def find_user(id)
-        user = User.joins(:person).select('users.id, username, first_name, middle_name, last_name, sex, date_of_birth, birth_date_estimated, users.is_active, users.voided, users.voided_reason').where(id:).first
+      def find_user(id, list: false)
+        user = user(id)
         return nil if user.nil?
 
-        roles = UserRoleMapping.joins(:user,
-                                      :role).where(user_id: id).select('roles.id, roles.name, user_role_mappings.retired, user_role_mappings.retired_reason, user_role_mappings.role_id')
-        departments = UserDepartmentMapping.joins(:user,
-                                                  :department).where(user_id: id).select('departments.id, departments.name, user_department_mappings.retired, user_department_mappings.retired_reason')
-        serialize(user, roles, departments)
+        roles = roles(id)
+        departments = departments(id)
+        serialize(user, roles, departments, list)
       end
 
       def username_exists?(username)
@@ -124,26 +139,32 @@ module UserManagement
         true
       end
 
+      # rubocop:disable Metrics/AbcSize
       # rubocop:disable Metrics/MethodLength
-      def serialize(user, roles, departments)
-        {
-          id: user.id,
-          username: user.username,
-          first_name: user.first_name,
-          middle_name: user.middle_name,
-          last_name: user.last_name,
-          sex: user.sex,
-          is_active: user.is_active.zero?,
-          date_of_birth: user.date_of_birth,
-          birth_date_estimated: user.birth_date_estimated,
-          voided: user.voided,
-          voided_reason: user.voided_reason,
-          roles:,
-          permissions: user_privileges(user),
-          departments:,
-          lab_locations: lab_locations(user.id)
-        }
+      def serialize(user, roles, departments, list)
+        user_odt =
+          {
+            id: user.id,
+            username: user.username,
+            first_name: user.first_name,
+            middle_name: user.middle_name,
+            last_name: user.last_name,
+            sex: user.sex,
+            is_active: user.is_active.zero?,
+            date_of_birth: user.date_of_birth,
+            birth_date_estimated: user.birth_date_estimated,
+            voided: user.voided,
+            voided_reason: user.voided_reason
+          }
+        return user_odt if list
+
+        user_odt[:roles] = roles
+        user_odt[:departments] = departments
+        user_odt[:permissions] = user_privileges(user)
+        user_odt[:lab_locations] = lab_locations(user.id)
+        user_odt
       end
+      # rubocop:enable Metrics/AbcSize
       # rubocop:enable Metrics/MethodLength
 
       def user_privileges(user)
@@ -158,28 +179,58 @@ module UserManagement
 
       def users(query)
         @users = User.all
-        @users = User.search(params[:search]) if query.present?
+        @users = User.search(query) if query.present?
         @users
       end
 
+      # rubocop:disable Metrics/MethodLength
+      def user(id)
+        User.joins(:person)
+            .select('
+                   users.id,
+                   username,
+                   first_name,
+                   middle_name,
+                   last_name,
+                   sex,
+                   date_of_birth,
+                   birth_date_estimated,
+                   users.is_active,
+                   users.voided,
+                   users.voided_reason
+                   ')
+            .where(id:).first
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      def roles(user_id)
+        UserRoleMapping.joins(:role).where(user_id:)
+                       .select('
+                          roles.id,
+                          roles.name,
+                          user_role_mappings.retired,
+                          user_role_mappings.retired_reason,
+                          user_role_mappings.role_id
+                        ')
+      end
+
+      def departments(user_id)
+        UserDepartmentMapping.joins(:department).where(user_id:)
+                             .select('
+                                departments.id,
+                                departments.name,
+                                user_department_mappings.retired,
+                                user_department_mappings.retired_reason
+                             ')
+      end
+
       def serialize_users(users)
-        users_a = []
-        users.each do |user|
-          users_a.push({
-                         id: user.id,
-                         username: user.username,
-                         is_active: user.is_active == 0,
-                         first_name: user.person.first_name,
-                         middle_name: user.person.middle_name,
-                         last_name: user.person.last_name,
-                         sex: user.person.sex,
-                         date_of_birth: user.person.date_of_birth,
-                         birth_date_estimated: user.person.birth_date_estimated,
-                         create_date: user.person.created_date
-                       })
+        users.map do |user|
+          find_user(user.id, list: true)
         end
-        users_a
       end
     end
+    # rubocop:enable Metrics/ClassLength
   end
+  # rubocop:enable Metrics/ModuleLength
 end
