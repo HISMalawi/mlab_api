81e0630560854194b5d02d2ecdff9e70d2993621
Hopson Gausi <hopgausi20@gmail.com>
2024-08-27 14:55:39 +0200

    infection report drill down

--- app/services/reports/aggregate/infection.rb
+++ app/services/reports/aggregate/infection.rb
@@ -2,30 +2,35 @@ module Reports
   module Aggregate
     class Infection
       def generate_report(from: Date.today.to_s, to: Date.today.to_s, department: nil)
+        department = Department.find_by_name(department)&.id
         process_data(serliarize_data(get_data(from, to, department)))
       end
 
       def get_summary(from: Date.today.to_s, to: Date.today.to_s, department: nil)
-        department = department.present? ? " AND tt.department_id = #{department}" : ''
+        department = Department.find_by_name(department)
+        department_condition = department.present? ? " AND tt.department_id = #{department&.id}" : ''
+
         query = <<-SQL
-          SELECT tt.name, COUNT(DISTINCT t.id) AS count
+          SELECT tt.name, COUNT(DISTINCT t.id) AS count, GROUP_CONCAT(DISTINCT t.id) AS associated_ids
           FROM tests AS t
           JOIN test_types AS tt ON t.test_type_id = tt.id
           INNER JOIN
           test_statuses ts ON ts.test_id = t.id
           WHERE ts.status_id IN (4 , 5)
           AND (t.created_date BETWEEN '#{from.to_date.beginning_of_day}' AND '#{to.to_date.end_of_day}')
-          #{department}
+          #{department_condition}
           GROUP BY tt.name
         SQL
-        Report.find_by_sql(query).map(&:attributes)
+        ActiveRecord::Base.connection.execute('SET SESSION group_concat_max_len = 1000000')
+        serliarize_summary(Report.find_by_sql(query), department&.name)
       end
 
       private
 
       def get_data(from, to, department)
         condition = " DATE(t.created_date) BETWEEN '#{from.to_date.beginning_of_day}' and '#{to.to_date.end_of_day}'"
-        condition = condition << " AND tt.department_id = '#{department}'" unless department.nil?
+        condition <<= " AND tt.department_id = '#{department}'" unless department.nil?
+        ActiveRecord::Base.connection.execute('SET SESSION group_concat_max_len = 1000000')
         Report.find_by_sql(
           "SELECT DISTINCT
           tt.name AS test,
@@ -70,7 +75,8 @@ module Reports
               ELSE tr.value
           END AS result,
           p.sex AS gender,
-          COUNT(DISTINCT t.id) AS total
+          COUNT(DISTINCT t.id) AS total,
+          GROUP_CONCAT(DISTINCT t.id) AS associated_ids
       FROM
           tests t
               INNER JOIN
@@ -102,6 +108,20 @@ module Reports
         )
       end
 
+      def serliarize_summary(records, department)
+        data = []
+        records.each do |record|
+          data.push(
+            {
+              name: record['name'],
+              total: record['count'],
+              associated_ids: UtilsService.insert_drilldown(record, department)
+            }
+          )
+        end
+        data
+      end
+
       def serliarize_data(records)
         data = []
         records.each do |record|
@@ -112,7 +132,8 @@ module Reports
               indicator: record['measure'],
               age_group: record['age_group'],
               result: record['result'],
-              gender: record['gender']
+              gender: record['gender'],
+              associated_ids: record['associated_ids']
             }
           )
         end
@@ -128,6 +149,7 @@ module Reports
           age_group = test_[:age_group].to_s
           result = test_[:result].to_s
           gender = test_[:gender].to_s
+          associated_ids = UtilsService.insert_drilldown(test_, nil)
 
           test_hash[test_type] ||= {}
           test_hash[test_type][indicator] ||= []
@@ -138,9 +160,11 @@ module Reports
           age_group_hash ||= { age_group => {} }
           result_hash[result] << age_group_hash unless result_hash[result].include?(age_group_hash)
 
-          age_group_hash[age_group][gender] ||= 0
-          age_group_hash[age_group][gender] += total.to_i
+          age_group_hash[age_group][gender] ||= { count: 0, associated_ids: '' }
+          age_group_hash[age_group][gender][:count] += total.to_i
+          age_group_hash[age_group][gender][:associated_ids] = associated_ids
         end
+
         test_hash.map do |test_type, indicator|
           {
             test_type:,
@@ -152,22 +176,28 @@ module Reports
               {
                 name: measure,
                 results: result.map do |value, _|
-                           x = value.map do |key, hashes|
-                             { key => hashes.inject({}) do |acc, sub_hash|
-                               acc.merge(sub_hash) { |_, a, b| a.is_a?(Hash) && b.is_a?(Hash) ? a.merge(b) : [a, b] }
-                             end }
-                           end
-                           x.map do |result_|
-                             {
-                               result_.keys.first => {
-                                 'F' => result_.values.first.map do |age_group, genders|
-                                          [age_group, genders['F']]
-                                        end.to_h,
-                                 'M' => result_.values.first.map { |age_group, genders| [age_group, genders['M']] }.to_h
-                               }
-                             }
-                           end
-                         end
+                  x = value.map do |key, hashes|
+                    { key => hashes.inject({}) do |acc, sub_hash|
+                      acc.merge(sub_hash) { |_, a, b| a.is_a?(Hash) && b.is_a?(Hash) ? a.merge(b) : [a, b] }
+                    end }
+                  end
+                  x.map do |result_|
+                    {
+                      result_.keys.first => {
+                        'F' => result_.values.first.map do |age_group, genders|
+                          [age_group,
+                           { count: genders['F']&.dig(:count) || 0,
+                             associated_ids: genders['F']&.dig(:associated_ids) || '' }]
+                        end.to_h,
+                        'M' => result_.values.first.map do |age_group, genders|
+                          [age_group,
+                           { count: genders['M']&.dig(:count) || 0,
+                             associated_ids: genders['M']&.dig(:associated_ids) || '' }]
+                        end.to_h
+                      }
+                    }
+                  end
+                end
               }
             end
           }
