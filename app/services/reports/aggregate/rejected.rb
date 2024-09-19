1eb9550660c95c09f61f4a1d24eea0fbef5cac90
Hopson Gausi <hopgausi20@gmail.com>
2024-09-19 10:34:30 +0200

    Fix rejected report failing to fetch data

--- app/services/reports/aggregate/rejected.rb
+++ app/services/reports/aggregate/rejected.rb
@@ -8,8 +8,7 @@ module Reports
     module Rejected
       class << self
         def generate_report(from: nil, to: nil, department: nil)
-          department = Department.find_by(name: department)
-          department_condition = department.present? ? "AND test_types.department_id = #{department&.id}" : ''
+          department_condition = department.present? ? "AND test_types.department_id = #{department}" : ''
           query = build_query(from, to, department_condition)
           ActiveRecord::Base.connection.execute('SET SESSION group_concat_max_len = 1000000')
           rejected_tests = ActiveRecord::Base.connection.execute(query)
@@ -21,6 +20,7 @@ module Reports
         private
 
         def result_by_ward(rejected_tests, department)
+          department = Department.find_by(id: department)
           rejected_tests.each_with_object({}) do |(reason_name, test_type, test_count, ward, associated_ids), result|
             reason = result[reason_name] ||= { name: reason_name, test_types: [] }
             associated_ids = UtilsService.insert_drilldown({ associated_ids: }, department&.name)
@@ -35,7 +35,10 @@ module Reports
         def build_query(from, to, department_condition)
           <<~SQL
             SELECT
-              status_reasons.description AS reason_name,
+              COALESCE(
+                MAX(status_reasons.description),
+                MAX(sr.description)
+              ) AS reason_name,
               test_types.name AS test_type,
               COUNT(DISTINCT t.id) AS test_count,
               fs.name AS ward,
@@ -45,16 +48,18 @@ module Reports
               JOIN orders o ON t.order_id = o.id
               JOIN encounters e ON e.id = o.encounter_id
               JOIN facility_sections fs ON fs.id = e.facility_section_id
-              JOIN test_statuses ON test_statuses.test_id = t.id
-              JOIN statuses ON statuses.id = test_statuses.status_id
-              JOIN status_reasons ON status_reasons.id = test_statuses.status_reason_id
               JOIN test_types ON test_types.id = t.test_type_id
+              LEFT JOIN test_statuses ON test_statuses.test_id = t.id
+              LEFT JOIN statuses ON statuses.id = t.status_id
+              LEFT JOIN status_reasons ON status_reasons.id = test_statuses.status_reason_id
+              LEFT JOIN order_statuses ON order_statuses.order_id = o.id
+              LEFT JOIN statuses as s ON s.id = o.status_id
+              LEFT JOIN status_reasons sr ON sr.id = order_statuses.status_reason_id
             WHERE
-              statuses.name LIKE '%rejected%'
+              (statuses.name = 'test-rejected' OR s.name = 'specimen-rejected')
               #{department_condition}
               AND (DATE(o.created_date) BETWEEN '#{from}' AND '#{to}')
-            GROUP BY
-              status_reasons.description, test_types.name, fs.name;
+            GROUP BY test_types.name, fs.name;
           SQL
         end
       end
