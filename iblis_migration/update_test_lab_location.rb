0aecc0671fb14948329d2edbef9194d5d6df3dd1
Hopson Gausi <hopgausi20@gmail.com>
2025-01-27 14:07:35 +0200

    Add user and test location only for those that do not have already

--- iblis_migration/update_test_lab_location.rb
+++ iblis_migration/update_test_lab_location.rb
@@ -5,17 +5,17 @@ cancer_test_types = TestType.active_with_cancer.pluck(:id)
 paed = LabLocation.find_by_name('Paediatric Lab').id
 cancer = LabLocation.find_by_name('cancer lab').id
 puts 'Updating paeds tests...'
-Test.where(test_type_id: paeds_test_types).update_all(lab_location_id: paed)
-orders_ids = Test.where(test_type_id: paeds_test_types).pluck(:order_id)
+Test.where(test_type_id: paeds_test_types, lab_location_id: nil).update_all(lab_location_id: paed)
+orders_ids = Test.where(test_type_id: paeds_test_types, lab_location_id: nil).pluck(:order_id)
 encounters_ids = Order.where(id: orders_ids)
 clients_ids = Encounter.where(id: encounters_ids)
-Client.where(id: clients_ids).update_all(lab_location_id: paed)
+Client.where(id: clients_ids, lab_location_id: nil).update_all(lab_location_id: paed)
 puts 'Updating cancer tests...'
-Test.where(test_type_id: cancer_test_types).update_all(lab_location_id: cancer)
+Test.where(test_type_id: cancer_test_types, lab_location_id: nil).update_all(lab_location_id: cancer)
 orders_ids = Test.where(test_type_id: cancer_test_types).pluck(:order_id)
 encounters_ids = Order.where(id: orders_ids)
 clients_ids = Encounter.where(id: encounters_ids)
-Client.where(id: clients_ids).update_all(lab_location_id: cancer)
+Client.where(id: clients_ids, lab_location_id: nil).update_all(lab_location_id: cancer)
 puts 'Refreshing home analystics ...'
 HomeDashboard.delete_all
 HomeDashboardJob.perform_async
